3cfff4abb15a5edeb320b63eb67ec769c6bfdc79
Michał Nowacki <michal.nowacki@reef.pl>
2024-02-02 04:02:27 +0100

    touch /tmp/celery-beat.log

--- validator/app/envs/prod/celery-beat-entrypoint.sh
+++ validator/app/envs/prod/celery-beat-entrypoint.sh
@@ -3,6 +3,8 @@ set -eu
 
 type prometheus-cleanup.sh && ./prometheus-cleanup.sh
 
+touch /tmp/celery-beat.log
+
 nice celery -A compute_horde_validator beat -l INFO --schedule /tmp/celerybeat-schedule -f /tmp/celery-beat.log &
 pid=$!
 
