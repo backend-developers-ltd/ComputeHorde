bb6944c6d580e704e91915e5199bcd54b476ffe9
Michał Nowacki <michal.nowacki@reef.pl>
2024-02-02 01:03:24 +0100

    that's an embarassing typo

--- miner/app/build-image.sh
+++ miner/app/build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
-IMAGE_NAME="backenddevelopersltd/computete-horde-miner:v0-latest"
+IMAGE_NAME="backenddevelopersltd/compute-horde-miner:v0-latest"
 rsync -avzP ../../compute_horde packages/
 docker build -t $IMAGE_NAME -f envs/prod/Dockerfile .
