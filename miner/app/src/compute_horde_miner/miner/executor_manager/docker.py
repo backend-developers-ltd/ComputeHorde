645a997ea9f1ec40ebfd156c7dc01cc56ffa4005
Michał Nowacki <michal.nowacki@reef.pl>
2024-02-06 14:58:25 +0100

    pull executor before running it

--- miner/app/src/compute_horde_miner/miner/executor_manager/docker.py
+++ miner/app/src/compute_horde_miner/miner/executor_manager/docker.py
@@ -5,6 +5,9 @@ from django.conf import settings
 from compute_horde_miner.miner.executor_manager.base import BaseExecutorManager
 
 
+EXECUTOR_IMAGE = "backenddevelopersltd/compute-horde-executor:v0-latest"
+
+
 class DockerExecutorManager(BaseExecutorManager):
     def reserve_executor(self, token):
         if settings.ADDRESS_FOR_EXECUTORS:
@@ -17,6 +20,7 @@ class DockerExecutorManager(BaseExecutorManager):
                 '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}',
                 'root_app_1'
             ]).decode().strip()
+        subprocess.check_call(['docker', 'pull', EXECUTOR_IMAGE])
         subprocess.Popen([  # noqa: S607
             "docker", "run", "--rm",
             "-e", f"MINER_ADDRESS=ws://{address}:{settings.PORT_FOR_EXECUTORS}",
@@ -24,6 +28,6 @@ class DockerExecutorManager(BaseExecutorManager):
             # the executor must be able to spawn images on host
             "-v", "/var/run/docker.sock:/var/run/docker.sock",
             "-v", "/tmp:/tmp",
-            "backenddevelopersltd/compute-horde-executor:v0-latest",
+            EXECUTOR_IMAGE,
             "python", "manage.py", "run_executor",
         ])
