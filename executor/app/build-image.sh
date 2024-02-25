12df72a24d69ca64110de67dfbb50cb678af4f4c
Vykintas Baltrušaitis <vykintas.baltrusaitis@reef.pl>
2024-02-26 01:11:58 +0200

    Fix Dockerfiles to work with pdm setup

--- executor/app/build-image.sh
+++ executor/app/build-image.sh
@@ -2,4 +2,5 @@
 
 IMAGE_NAME="backenddevelopersltd/compute-horde-executor:v0-latest"
 rsync -avzP ../../compute_horde packages/
-docker build -t $IMAGE_NAME -f envs/prod/Dockerfile .
+# Dockerfile assumes that build is run in the parent dir
+cd .. && docker build -t $IMAGE_NAME -f app/envs/prod/Dockerfile .
