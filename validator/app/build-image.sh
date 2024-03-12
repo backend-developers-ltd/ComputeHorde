a076f2e353007da5961314cd336090e9c00012a4
Enam Mijbah Noor <enam.noor@reef.pl>
2024-03-12 12:37:56 +0600

    Undo build-image.sh files

--- validator/app/build-image.sh
+++ validator/app/build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
-: "${IMAGE_NAME:=backenddevelopersltd/compute-horde-validator:v0-latest}"
+IMAGE_NAME="backenddevelopersltd/compute-horde-validator:v0-latest"
 # Dockerfile assumes that build is run in the parent dir
 cd .. && docker build --platform linux/amd64 --build-context compute-horde=../compute_horde  -t $IMAGE_NAME -f app/envs/prod/Dockerfile .
