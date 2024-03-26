bf13293390e5c97c70bd0c9f9e76111aef740d5d
Enam Mijbah Noor <enam.noor@reef.pl>
2024-03-26 20:54:00 +0600

    Use docker cli image as base for runner images
    It includes docker cli v26 and compose v2. Previous docker v24 & compose v1 was conflicting with host's docker, if the host docker version is >=26

--- validator/envs/runner/build-image.sh
+++ validator/envs/runner/build-image.sh
@@ -2,4 +2,4 @@
 
 IMAGE_NAME="backenddevelopersltd/compute-horde-validator-runner:v0-latest"
 rsync -avzP ../../nginx/config_helpers data/nginx/
-docker build -t $IMAGE_NAME .
+docker build --platform=linux/amd64 -t $IMAGE_NAME .
