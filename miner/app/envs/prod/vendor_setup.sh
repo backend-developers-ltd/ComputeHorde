a2a8d507da2d2aac5564ee6d58e8b9b13ebcd107
Michal Zukowski <michal.zukowski@reef.pl>
2024-04-15 08:40:22 +0200

    Support miner custom code

---
+++ miner/app/envs/prod/vendor_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Check if VENDOR_DIRECTORY is set
+if [ -z "${VENDOR_DIRECTORY}" ]; then
+  echo "VENDOR_DIRECTORY is not set."
+else
+  # Check if VENDOR_DIRECTORY exists
+  if [ ! -d "${VENDOR_DIRECTORY}" ]; then
+    echo "VENDOR_DIRECTORY does not exist: ${VENDOR_DIRECTORY}"
+  else
+    # Add VENDOR_DIRECTORY to PYTHONPATH
+    export PYTHONPATH="${VENDOR_DIRECTORY}:${PYTHONPATH}"
+    # Check if setup.sh exists in VENDOR_DIRECTORY
+    if [ -f "${VENDOR_DIRECTORY}/setup.sh" ]; then
+      # Run setup.sh
+      . "${VENDOR_DIRECTORY}/setup.sh"
+    fi
+  fi
+fi
