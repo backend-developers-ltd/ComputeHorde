3dc9810a9d224255c030c0bcde2ec14360218379
Andreea Popescu <andreea.popescu@reef.pl>
2024-04-17 14:54:03 +0100

    chained synthetic job

--- validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/gpu_hashcat.py
+++ validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/gpu_hashcat.py
@@ -1,85 +1,81 @@
+import bittensor
 from compute_horde.mv_protocol.miner_requests import V0JobFinishedRequest
+from django.conf import settings
 
-from compute_horde_validator.validator.jobs import Algorithm, V0SyntheticJob
 from compute_horde_validator.validator.synthetic_jobs.generator.base import (
     AbstractSyntheticJobGenerator,
 )
+from compute_horde_validator.validator.synthetic_jobs.synthetic_job import (
+    HASHJOB_PARAMS,
+    Algorithm,
+)
+from compute_horde_validator.validator.synthetic_jobs.v0_synthetic_job import V0SyntheticJob
+from compute_horde_validator.validator.synthetic_jobs.v1_synthetic_job import V1SyntheticJob
 from compute_horde_validator.validator.utils import single_file_zip
 
 MAX_SCORE = 2
 
 
-class GPUHashcatSyntheticJobGenerator(AbstractSyntheticJobGenerator):
-    algorithms = [Algorithm.SHA256, Algorithm.SHA384, Algorithm.SHA512]
+def get_subnet_weights_version():
+    try:
+        subtensor = bittensor.subtensor(network=settings.BITTENSOR_NETWORK)
+        hyperparameters = subtensor.get_subnet_hyperparameters(netuid=settings.BITTENSOR_NETUID)
+        if hyperparameters is None:
+            raise RuntimeError("Network hyperparameters are None")
+        return hyperparameters.weights_version
+    except Exception as e:
+        raise RuntimeError(f"Failed to get subnet weight version: {e}")
 
+
+class GPUHashcatSyntheticJobGenerator(AbstractSyntheticJobGenerator):
     def __init__(self):
-        self.algorithm = self.get_algorithm()
-        self.password_length, self.hashcat_hash_type, self._timeout = self.algo_to_params(self.algorithm)
-        self.hash_job = V0SyntheticJob.generate(self.algorithm, password_length=self.password_length)
+        # set synthetic_jobs based on subnet weights_version
+        self.weights_version = get_subnet_weights_version()
+        if self.weights_version == 0:
+            algorithm = Algorithm.get_random_algorithm()
+            self.hash_job = V0SyntheticJob.generate(
+                algorithm, HASHJOB_PARAMS[self.weights_version][algorithm]
+            )
+        elif self.weights_version == 1:
+            algorithms = Algorithm.get_all_algorithms()
+            params = [HASHJOB_PARAMS[self.weights_version][algorithm] for algorithm in algorithms]
+            self.hash_job = V1SyntheticJob.generate(algorithms, params)
+        else:
+            raise RuntimeError(f"No SyntheticJob for weights_version: {self.weights_version}")
+
         self.expected_answer = self.hash_job.answer
 
-    @classmethod
-    def get_algorithm(cls) -> Algorithm:
-        return cls.algorithms[datetime.datetime.utcnow().hour % len(cls.algorithms)]
-
-    @classmethod
-    def algo_to_params(cls, algo: Algorithm):
-        if algo == Algorithm.SHA256:
-            return 6, "1410", 90
-        elif algo == Algorithm.SHA384:
-            return 5, "10810", 45
-        elif algo == Algorithm.SHA512:
-            return 5, "1710", 45
-        else:
-            raise ValueError(f"Invalid algorithm: {algo}")
-
     def timeout_seconds(self) -> int:
-        return self._timeout
+        return self.hash_job.timeout_seconds
 
     def base_docker_image_name(self) -> str:
-        return "backenddevelopersltd/compute-horde-job:v0-latest"
+        return f"backenddevelopersltd/compute-horde-job:v{self.weights_version}-latest"
 
     def docker_image_name(self) -> str:
-        return "backenddevelopersltd/compute-horde-job:v0-latest"
+        return f"backenddevelopersltd/compute-horde-job:v{self.weights_version}-latest"
 
     def docker_run_options_preset(self) -> str:
-        return 'nvidia_all'
+        return "nvidia_all"
 
     def docker_run_cmd(self) -> list[str]:
-        return [
-            "--runtime",
-            "600",
-            "--restore-disable",
-            "--attack-mode",
-            "3",
-            "--workload-profile",
-            "3",
-            "--optimized-kernel-enable",
-            "--hash-type",
-            self.hashcat_hash_type,
-            "--hex-salt",
-            "-1",
-            "?l?d?u",
-            "--outfile-format",
-            "2",
-            "--quiet",
-            self.hash_job.payload,
-            "?1" * self.password_length,
-        ]
+        return self.hash_job.docker_run_cmd()
+
+    def raw_script(self) -> str | None:
+        return self.hash_job.raw_script()
 
     def volume_contents(self) -> str:
-        return single_file_zip('payload.txt', self.hash_job.payload)
+        return single_file_zip("payload.txt", self.hash_job.payload)
 
     def verify(self, msg: V0JobFinishedRequest, time_took: float) -> tuple[bool, str, float]:
         if msg.docker_process_stdout.strip() != self.expected_answer:
             return (
                 False,
-                f'result does not match expected answer: expected answer={self.expected_answer} msg={msg.json()}',
+                f"result does not match expected answer: expected answer={self.expected_answer} msg={msg.json()}",
                 0,
             )
+        # TODO (timeout -time_taken) **2 ?
         score = MAX_SCORE * (1 - (time_took / (2 * self.timeout_seconds())))
-        return True, '', score
+        return True, "", score
 
-    def job_description(self):
-        return (f'Hashcat {self.algorithm.value}, password length = {self.password_length}, '
-                f'timeout={self.timeout_seconds()}s')
+    def job_description(self) -> str:
+        return f"Hashcat {self.hash_job}"
---
+++ validator/app/src/compute_horde_validator/validator/synthetic_jobs/v1_decrypt.py
@@ -0,0 +1,69 @@
+import hashlib
+import pickle
+import subprocess
+from base64 import b64encode
+
+from cryptography.fernet import Fernet
+
+DECRYPT_SCRIPT = """
+import hashlib
+import pickle
+import subprocess
+from base64 import b64encode
+
+from cryptography.fernet import Fernet
+
+with open("/volume/payload.txt", "rb") as file:
+    data = pickle.load(file)
+
+    answers = []
+    for i in range(int(data["n"])):
+        payload = data["payloads"][i]
+        mask = data["masks"][i]
+        algorithm = data["algorithms"][i]
+
+        if i > 0:
+            # decrypt payload with previous cracked passwords
+            passwords = "\\n".join(answers[-1]).encode("utf-8")
+            key = b64encode(hashlib.sha256(passwords).digest(), altchars=b"-_")
+            payload = Fernet(key).decrypt(payload).decode("utf-8")
+
+        with open("_payload.txt", mode="wb") as f:
+            f.write(payload.encode("utf-8"))
+
+        cmd = f'hashcat --runtime 99600 --potfile-disable --restore-disable --attack-mode 3 --workload-profile 3 --optimized-kernel-enable --hash-type {algorithm} --hex-salt -1 "?l?d?u" --outfile-format 2 --quiet _payload.txt "{mask}"'
+        passwords = subprocess.check_output(cmd, shell=True, text=True)
+        passwords = sorted(passwords.split("\\n"))[1:]
+        answers.append(passwords)
+    print(answers)
+"""
+
+
+def main():
+    with open("/volume/payload.txt", "rb") as file:
+        data = pickle.load(file)
+
+        answers = []
+        for i in range(int(data["n"])):
+            payload = data["payloads"][i]
+            mask = data["masks"][i]
+            algorithm = data["algorithms"][i]
+
+            if i > 0:
+                # decrypt payload with previous cracked passwords
+                passwords = '\n'.join(answers[-1]).encode("utf-8")
+                key = b64encode(hashlib.sha256(passwords).digest(), altchars=b"-_")
+                payload = Fernet(key).decrypt(payload).decode("utf-8")
+
+            with open("_payload.txt", mode="wb") as f:
+                f.write(payload.encode("utf-8"))
+
+            cmd = f'hashcat --potfile-disable --restore-disable --attack-mode 3 --workload-profile 3 --optimized-kernel-enable --hash-type {algorithm} --hex-salt -1 "?l?d?u" --outfile-format 2 --quiet _payload.txt "{mask}"'
+            passwords = subprocess.check_output(cmd, shell=True, text=True)
+            passwords = sorted(passwords.split('\n'))[1:]
+            answers.append(passwords)
+        print(answers)
+
+
+if __name__ == "__main__":
+    main()
