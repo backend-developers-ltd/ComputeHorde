fb8ba58b1c024ca8e0b46d2ed30df7bdf3883d1f
Enam Mijbah Noor <enam.noor@reef.pl>
2024-04-18 19:37:53 +0600

    Use official docker in installer scripts

--- install_miner.sh
+++ install_miner.sh
@@ -48,8 +48,24 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 # install docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+do
+  sudo apt-get remove $pkg || true
+done
+
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl docker.io docker-compose
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-compose-plugin
 sudo usermod -aG docker $USER
 
 # install cuda
@@ -119,6 +135,6 @@ ENDENV
 docker pull backenddevelopersltd/compute-horde-executor:v0-latest
 docker pull backenddevelopersltd/compute-horde-miner:v0-latest
 docker pull backenddevelopersltd/compute-horde-job:v0-latest
-docker-compose up -d
+docker compose up -d
 
 ENDSSH
