287a92697de118079aa45c0f842f7366c0ea8bca
Enam Mijbah Noor <enam.noor@reef.pl>
2024-04-26 04:42:40 +0600

    fix miner version endpoint

--- miner/app/src/compute_horde_miner/miner/views.py
+++ miner/app/src/compute_horde_miner/miner/views.py
@@ -4,5 +4,6 @@ from django.http import JsonResponse
 
 
 def get_version(request):
-    version = os.environ.get('MINER_VERSION', 'unknown')
-    return JsonResponse({'version': version})
+    miner_version = os.environ.get('MINER_VERSION', 'unknown')
+    runner_version = os.environ.get('MINER_RUNNER_VERSION', 'unknown')
+    return JsonResponse({'miner_version': miner_version, 'runner_version': runner_version})
