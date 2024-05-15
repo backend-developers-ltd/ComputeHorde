a741df4735d0d99fb3338cb26090561c3c5a73dc
Enam Mijbah Noor <enam.noor@reef.pl>
2024-05-15 13:09:59 +0600

    make local receipt store non-blocking

--- miner/app/src/compute_horde_miner/miner/receipt_store/local.py
+++ miner/app/src/compute_horde_miner/miner/receipt_store/local.py
@@ -1,6 +1,7 @@
 import pathlib
 import zipfile
 
+from asgiref.sync import sync_to_async
 from django.conf import settings
 
 from compute_horde_miner.miner.receipt_store.base import BaseReceiptStore, Receipt
@@ -8,18 +9,22 @@ from compute_horde_miner.miner.receipt_store.base import BaseReceiptStore, Recei
 ZIP_FILENAME = "receipts.zip"
 
 
+def _store(receipts: list[Receipt]):
+    if not receipts:
+        return
+
+    root = pathlib.Path(settings.LOCAL_RECEIPTS_ROOT)
+    root.mkdir(parents=True, exist_ok=True)
+    filepath = root / ZIP_FILENAME
+
+    with zipfile.ZipFile(filepath, mode='w', compression=zipfile.ZIP_LZMA) as zf:
+        for receipt in receipts:
+            zf.writestr(receipt.payload.job_uuid + '.json', receipt.json())
+
+
 class LocalReceiptStore(BaseReceiptStore):
     async def store(self, receipts: list[Receipt]):
-        if not receipts:
-            return
-
-        root = pathlib.Path(settings.LOCAL_RECEIPTS_ROOT)
-        root.mkdir(parents=True, exist_ok=True)
-        filepath = root / ZIP_FILENAME
-
-        with zipfile.ZipFile(filepath, mode='w', compression=zipfile.ZIP_LZMA) as zf:
-            for receipt in receipts:
-                zf.writestr(receipt.payload.job_uuid + '.json', receipt.json())
+        await sync_to_async(_store, thread_sensitive=False)(receipts)
 
     async def get_url(self) -> str:
         return settings.LOCAL_RECEIPTS_URL + ZIP_FILENAME
