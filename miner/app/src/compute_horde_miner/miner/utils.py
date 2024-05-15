05d2672670a125b5b66cf0799381acb18bb7c990
Enam Mijbah Noor <enam.noor@reef.pl>
2024-05-15 13:59:07 +0600

    Synchronize prepare_receipts with lock

--- miner/app/src/compute_horde_miner/miner/utils.py
+++ miner/app/src/compute_horde_miner/miner/utils.py
@@ -1,8 +1,14 @@
+import asyncio
+
 from compute_horde_miner.miner.models import JobReceipt
 from compute_horde_miner.miner.receipt_store.base import Receipt
 from compute_horde_miner.miner.receipt_store.current import receipts_store
 
 
+_prepare_receipts_lock = asyncio.Lock()
+
+
 async def prepare_receipts() -> None:
-    receipts = [Receipt.from_job_receipt(jr) async for jr in JobReceipt.objects.all()]
-    await receipts_store.store(receipts)
+    async with _prepare_receipts_lock:
+        receipts = [Receipt.from_job_receipt(jr) async for jr in JobReceipt.objects.all()]
+        await receipts_store.store(receipts)
