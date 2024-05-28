b402d7beca33f906064d2e06c8ae64b3e41255b9
Andreea Popescu <andreea.popescu@reef.pl>
2024-05-28 08:02:23 +0100

    test miner_driver

---
+++ validator/app/src/compute_horde_validator/validator/tests/test_miner_driver.py
@@ -0,0 +1,165 @@
+"""
+This test file is here always to indicate that everything was installed and the CI was able to run tests.
+It should always pass as long as all dependencies are properly installed.
+"""
+
+import asyncio
+import uuid
+
+import pytest
+from compute_horde.miner_client.base import BaseRequest
+from compute_horde.mv_protocol.miner_requests import (
+    V0DeclineJobRequest,
+    V0ExecutorReadyRequest,
+    V0JobFailedRequest,
+    V0JobFinishedRequest,
+)
+
+# from compute_horde_validator.validator.facilitator_client import JobRequest, OrganicJob
+from compute_horde_validator.validator.management.commands.connect_facilitator import (
+    JobRequest,
+    OrganicJob,
+)
+from compute_horde_validator.validator.miner_driver import run_miner_job
+from compute_horde_validator.validator.models import Miner
+from compute_horde_validator.validator.synthetic_jobs.utils import JobState, MinerClient
+
+WEBSOCKET_TIMEOUT = 10
+
+
+class MockMinerClient(MinerClient):
+    mock_job_state = False
+
+    def __init__(self, loop: asyncio.AbstractEventLoop, **args):
+        super().__init__(loop, **args)
+
+    def miner_url(self) -> str:
+        return "ws://miner"
+
+    async def await_connect(self):
+        return
+
+    def accepted_request_type(self) -> type[BaseRequest]:
+        return BaseRequest
+
+    def incoming_generic_error_class(self) -> type[BaseRequest]:
+        return BaseRequest
+
+    def outgoing_generic_error_class(self) -> type[BaseRequest]:
+        return BaseRequest
+
+    async def handle_message(self, msg):
+        pass
+
+    async def send_model(self, model):
+        pass
+
+    def get_job_state(self, job_uuid):
+        if self.mock_job_state:
+            job_state = JobState()
+            job_state.miner_ready_or_declining_future.set_result(
+                V0ExecutorReadyRequest(job_uuid=job_uuid)
+            )
+            job_state.miner_finished_or_failed_future.set_result(
+                V0JobFinishedRequest(
+                    job_uuid=job_uuid,
+                    docker_process_stdout="",
+                    docker_process_stderr="",
+                )
+            )
+            return job_state
+        return super().get_job_state(job_uuid)
+
+
+def get_dummy_job_request(uuid: str) -> JobRequest:
+    return JobRequest(
+        type="job.new",
+        uuid=uuid,
+        miner_hotkey="miner_hotkey",
+        docker_image="nvidia",
+        raw_script="print('hello world')",
+        args=[],
+        env={},
+        use_gpu=False,
+        input_url="fake.com/input",
+        output_url="fake.com/output",
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "futures_result,expected_job_status_updates,organic_job_status",
+    [
+        ((None, None), ["failed"], OrganicJob.Status.FAILED),
+        ((V0DeclineJobRequest, None), ["rejected"], OrganicJob.Status.FAILED),
+        ((V0ExecutorReadyRequest, None), ["accepted", "failed"], OrganicJob.Status.FAILED),
+        (
+            (V0ExecutorReadyRequest, V0JobFailedRequest),
+            ["accepted", "failed"],
+            OrganicJob.Status.FAILED,
+        ),
+        (
+            (V0ExecutorReadyRequest, V0JobFinishedRequest),
+            ["accepted", "completed"],
+            OrganicJob.Status.COMPLETED,
+        ),
+    ],
+)
+async def test_miner_driver(futures_result, expected_job_status_updates, organic_job_status):
+    miner, _ = await Miner.objects.aget_or_create(hotkey="miner_client")
+    job_uuid = str(uuid.uuid4())
+    job_request = get_dummy_job_request(job_uuid)
+
+    job = await OrganicJob.objects.acreate(
+        job_uuid=job_uuid,
+        miner=miner,
+        miner_address="irrelevant",
+        miner_address_ip_version=4,
+        miner_port=9999,
+        job_description="User job from facilitator",
+    )
+    miner_client = MockMinerClient(
+        loop=asyncio.get_event_loop(),
+        miner_address="ignore",
+        my_hotkey="ignore",
+        miner_hotkey="ignore",
+        miner_port=9999,
+        job_uuid=job_uuid,
+        keypair=None,
+    )
+    f0, f1 = futures_result
+    if f0:
+        miner_client.job_states[job_uuid].miner_ready_or_declining_future.set_result(
+            f0(job_uuid=job_uuid)
+        )
+    if f1:
+        miner_client.job_states[job_uuid].miner_finished_or_failed_future.set_result(
+            f1(
+                job_uuid=job_uuid,
+                docker_process_stdout="",
+                docker_process_stderr="",
+            )
+        )
+
+    job_status_updates = []
+
+    async def track_job_status_updates(x):
+        job_status_updates.append(x)
+
+    await run_miner_job(
+        miner_client,
+        job,
+        job_request,
+        total_job_timeout=1,
+        wait_timeout=1,
+        notify_callback=track_job_status_updates,
+    )
+
+    assert len(job_status_updates) == len(expected_job_status_updates)
+    for job_status, expected_status in zip(job_status_updates, expected_job_status_updates):
+        assert job_status.status == expected_status
+        assert job_status.uuid == job_uuid
+
+    job = await OrganicJob.objects.aget(job_uuid=job_uuid)
+    assert job.status == organic_job_status
