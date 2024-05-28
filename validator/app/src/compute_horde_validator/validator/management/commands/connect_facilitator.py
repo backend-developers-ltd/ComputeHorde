ddaf40669048f99a690a67b000305c412ad9b009
Andreea Popescu <andreea.popescu@reef.pl>
2024-05-28 15:27:31 +0100

    add back connect_facilitator

---
+++ validator/app/src/compute_horde_validator/validator/management/commands/connect_facilitator.py
@@ -0,0 +1,20 @@
+import logging
+
+from asgiref.sync import async_to_sync
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from compute_horde_validator.validator.facilitator_client import FacilitatorClient
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    FACILITATOR_CLIENT_CLASS = FacilitatorClient
+
+    @async_to_sync
+    async def handle(self, *args, **options):
+        keypair = settings.BITTENSOR_WALLET().get_hotkey()
+        facilitator_client = self.FACILITATOR_CLIENT_CLASS(keypair, settings.FACILITATOR_URI)
+        async with facilitator_client:
+            await facilitator_client.run_forever()
