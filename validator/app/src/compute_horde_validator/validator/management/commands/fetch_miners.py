073fb556b1d1bc161aa7127745469c97ee1131c0
Enam Mijbah Noor <enam.noor@reef.pl>
2024-06-26 20:34:08 +0600

    fix formatting and tests

--- validator/app/src/compute_horde_validator/validator/management/commands/fetch_miners.py
+++ validator/app/src/compute_horde_validator/validator/management/commands/fetch_miners.py
@@ -7,6 +7,8 @@ from compute_horde_validator.validator.synthetic_jobs.utils import get_miners
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        metagraph = bittensor.metagraph(netuid=settings.BITTENSOR_NETUID, network=settings.BITTENSOR_NETWORK)
+        metagraph = bittensor.metagraph(
+            netuid=settings.BITTENSOR_NETUID, network=settings.BITTENSOR_NETWORK
+        )
         miners = get_miners(metagraph)
         print("Total miners:", len(miners))
