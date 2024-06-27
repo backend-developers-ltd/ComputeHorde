8318aed97699af8824428b618f385e92443523a9
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-06-27 19:28:58 +0300

    extracted job_score module

--- validator/app/src/compute_horde_validator/validator/tasks.py
+++ validator/app/src/compute_horde_validator/validator/tasks.py
@@ -32,6 +32,7 @@ from compute_horde_validator.validator.models import (
     SyntheticJobBatch,
     SystemEvent,
 )
+from compute_horde_validator.validator.synthetic_jobs import job_score
 from compute_horde_validator.validator.synthetic_jobs.utils import (
     MinerClient,
     create_and_run_sythethic_job_batch,
@@ -213,37 +214,6 @@ def get_metagraph(subtensor, netuid):
         return subtensor.metagraph(netuid=netuid)
 
 
-def sigmoid(x, beta, delta):
-    return 1 / (1 + np.exp(beta * (-x + delta)))
-
-
-def reversed_sigmoid(x, beta, delta):
-    return sigmoid(-x, beta=beta, delta=-delta)
-
-
-def horde_score(benchmarks, alpha=0, beta=0, delta=0):
-    """Proportionally scores horde benchmarks allowing increasing significance for chosen features
-
-    By default scores are proportional to horde "strength" - having 10 executors would have the same
-    score as separate 10 single executor miners. Subnet owner can control significance of defined features:
-
-    alpha - controls significance of average score, so smaller horde can have higher score if executors are stronger;
-            best values are from range [0, 1], with 0 meaning no effect
-    beta - controls sigmoid function steepness; sigmoid function is over `-(1 / horde_size)`, so larger hordes can be
-           more significant than smaller ones, even if summary strength of a horde is the same;
-           best values are from range [0,5] (or more, but higher values does not change sigmoid steepnes much),
-           with 0 meaning no effect
-    delta - controls where sigmoid function has 0.5 value allowing for better control over effect of beta param;
-            best values are from range [0, 1]
-    """
-    sum_agent = sum(benchmarks)
-    inverted_n = 1 / len(benchmarks)
-    avg_benchmark = sum_agent * inverted_n
-    scaled_inverted_n = reversed_sigmoid(inverted_n, beta=beta, delta=delta)
-    scaled_avg_benchmark = avg_benchmark**alpha
-    return scaled_avg_benchmark * sum_agent * scaled_inverted_n
-
-
 @app.task
 def set_scores():
     with save_event_on_error(SystemEvent.EventSubType.GENERIC_ERROR):
@@ -261,7 +231,6 @@ def set_scores():
             metagraph = get_metagraph(subtensor, netuid=settings.BITTENSOR_NETUID)
             neurons = metagraph.neurons
             hotkey_to_uid = {n.hotkey: n.uid for n in neurons}
-            score_per_uid = {}
             batches = list(
                 SyntheticJobBatch.objects.prefetch_related("synthetic_jobs").filter(
                     scored=False,
@@ -273,13 +242,7 @@ def set_scores():
                 logger.info("No batches - nothing to score")
                 return
 
-            # scaling factor for avg_score of a horde - best in range [0, 1] (0 means no effect on score)
-            alpha = settings.HORDE_SCORE_AVG_PARAM
-            # sigmoid steepnes param - best in range [0, 5] (0 means no effect on score)
-            beta = settings.HORDE_SCORE_SIZE_PARAM
-            # horde size for 0.5 value of sigmoid - sigmoid is for 1 / horde_size
-            central_horde_size = settings.HORDE_SCORE_CENTRAL_SIZE_PARAM
-            delta = 1 / central_horde_size
+            multi_batch_scores = []
             for batch in batches:
                 batch_scores = defaultdict(list)
                 for job in batch.synthetic_jobs.all():
@@ -287,19 +250,16 @@ def set_scores():
                     if uid is None:
                         continue
                     batch_scores[uid].append(job.score)
-                for uid, uid_batch_scores in batch_scores.items():
-                    uid_horde_score = horde_score(
-                        uid_batch_scores, alpha=alpha, beta=beta, delta=delta
-                    )
-                    score_per_uid[uid] = score_per_uid.get(uid, 0) + uid_horde_score
-            if not score_per_uid:
-                logger.info("No miners on the subnet to score")
-                return
+                multi_batch_scores.append(batch_scores)
+
             uids = np.zeros(len(neurons), dtype=np.int64)
-            weights = np.zeros(len(neurons), dtype=np.float32)
             for ind, n in enumerate(neurons):
                 uids[ind] = n.uid
-                weights[ind] = score_per_uid.get(n.uid, 0)
+
+            weights = job_score.compute_weights(multi_batch_scores, uids)
+            if not weights:
+                logger.info("No miners on the subnet to score")
+                return
 
             uids, weights = process_weights_for_netuid(
                 uids,
