24f3565582aa5215de6383984a4281852aa3265a
Andreea Popescu <andreea.popescu@reef.pl>
2024-06-27 07:44:32 +0100

    fix wallet mock

--- validator/app/src/compute_horde_validator/validator/tests/conftest.py
+++ validator/app/src/compute_horde_validator/validator/tests/conftest.py
@@ -1,6 +1,10 @@
+import logging
 from collections.abc import Generator
 
+import bittensor
 import pytest
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -8,3 +12,14 @@ def some() -> Generator[int, None, None]:
     # setup code
     yield 1
     # teardown code
+
+
+@pytest.fixture(scope="session", autouse=True)
+def wallet():
+    wallet = bittensor.wallet(name="test_validator")
+    try:
+        # workaround the overwrite flag
+        wallet.create_new_coldkey(n_words=12, use_password=False, overwrite=True)
+        wallet.create_new_hotkey(n_words=12, use_password=False, overwrite=True)
+    except Exception as e:
+        logger.error(f"Failed to create wallet: {e}")
