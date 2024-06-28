92d2dbd5a110e4208a04d2db53765f4d8153b54a
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-06-28 18:53:17 +0300

    remove obsolete demo organic jobs

--- validator/run_organic_job_v2/download_models.py
+++
@@ -1,7 +0,0 @@
-from transformers import BlipForConditionalGeneration, BlipProcessor
-
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-
-processor.save_pretrained("./Salesforce__blip-image-captioning-base")
-model.save_pretrained("./Salesforce__blip-image-captioning-base")
