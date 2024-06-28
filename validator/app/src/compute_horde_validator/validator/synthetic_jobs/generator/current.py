d298947b5554add6905d409002e3b1bed4f890b7
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-06-28 19:21:21 +0300

    rename AbstractSyntheticJobGenerator to Base... to match other uses

--- validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/current.py
+++ validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/current.py
@@ -3,9 +3,9 @@ import importlib
 from django.conf import settings
 
 from compute_horde_validator.validator.synthetic_jobs.generator.base import (
-    AbstractSyntheticJobGenerator,
+    BaseSyntheticJobGenerator,
 )
 
 module_path, class_name = settings.SYNTHETIC_JOB_GENERATOR.split(":", 1)
 target_module = importlib.import_module(module_path)
-SyntheticJobGenerator: type[AbstractSyntheticJobGenerator] = getattr(target_module, class_name)
+SyntheticJobGenerator: type[BaseSyntheticJobGenerator] = getattr(target_module, class_name)
