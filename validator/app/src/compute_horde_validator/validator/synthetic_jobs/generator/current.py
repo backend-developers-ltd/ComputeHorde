04568fa63a2f5e3ec12c22a9ef097daff9c296ca
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-06-28 20:01:52 +0300

    added SyntheticJobGeneratorFactory

--- validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/current.py
+++ validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/current.py
@@ -3,9 +3,11 @@ import importlib
 from django.conf import settings
 
 from compute_horde_validator.validator.synthetic_jobs.generator.base import (
-    BaseSyntheticJobGenerator,
+    BaseSyntheticJobGeneratorFactory,
 )
 
-module_path, class_name = settings.SYNTHETIC_JOB_GENERATOR.split(":", 1)
+module_path, class_name = settings.SYNTHETIC_JOB_GENERATOR_FACTORY.split(":", 1)
 target_module = importlib.import_module(module_path)
-SyntheticJobGenerator: type[BaseSyntheticJobGenerator] = getattr(target_module, class_name)
+klass = getattr(target_module, class_name)
+
+synthetic_job_generator_factory: BaseSyntheticJobGeneratorFactory = klass()
