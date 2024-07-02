6da6b9ffb143bae8decb730039d10325989898c2
Enam Mijbah Noor <enam.noor@reef.pl>
2024-07-02 12:10:06 +0600

    Use remote miner running tests

---
+++ scripts/e2e/check_miner_version.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+import sys
+import time
+from contextlib import suppress
+from urllib.request import urlopen
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--miner-ip", required=True)
+    parser.add_argument("--miner-port", required=True)
+    parser.add_argument("--expected-version", required=True)
+    args = parser.parse_args()
+
+    for i in range(10):
+        with suppress(Exception):
+            time.sleep(i)
+            response = urlopen(f"http://{args.miner_ip}:{args.miner_port}/version", timeout=10)
+            data = json.loads(response.read())
+            if data["miner_version"] == args.expected_version:
+                return 0
+
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
