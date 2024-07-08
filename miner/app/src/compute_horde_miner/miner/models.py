024d44169e99327026f050b959a24883005ef09a
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-07-09 02:33:28 +0500

    Add debug boolean flag to Validator model

--- miner/app/src/compute_horde_miner/miner/models.py
+++ miner/app/src/compute_horde_miner/miner/models.py
@@ -19,6 +19,7 @@ class EnumEncoder(DjangoJSONEncoder):
 class Validator(models.Model):
     public_key = models.TextField(unique=True)
     active = models.BooleanField()
+    debug = models.BooleanField(default=False)
 
     def __str__(self):
         return f"hotkey: {self.public_key}"
