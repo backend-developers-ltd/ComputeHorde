e0a89a3cdb82b01e3846baa443ec52aa12a4450a
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-07-13 23:58:28 +0500

    Add integration test for miner decline job message

--- miner/app/src/compute_horde_miner/miner/tests/integration/test_mocked_executor_manager.py
+++ miner/app/src/compute_horde_miner/miner/tests/integration/test_mocked_executor_manager.py
@@ -2,29 +2,48 @@ import time
 import uuid
 
 import pytest
+import pytest_asyncio
 from channels.testing import WebsocketCommunicator
 from compute_horde.executor_class import DEFAULT_EXECUTOR_CLASS
+from pytest_mock import MockerFixture
 
 from compute_horde_miner import asgi
+from compute_horde_miner.miner.executor_manager.base import ExecutorUnavailable
 from compute_horde_miner.miner.models import Validator
 from compute_horde_miner.miner.tests.executor_manager import fake_executor
 
 WEBSOCKET_TIMEOUT = 10
 
 
-@pytest.mark.asyncio
-@pytest.mark.django_db(transaction=True)
-async def test_main_loop():
-    validator_key = "some_public_key"
-    await Validator.objects.acreate(public_key=validator_key, active=True)
+@pytest.fixture
+def validator_key():
+    return "some_public_key"
 
-    job_uuid = str(uuid.uuid4())
-    fake_executor.job_uuid = job_uuid
-    communicator = WebsocketCommunicator(
+
+@pytest_asyncio.fixture(autouse=True)
+async def validator(validator_key: str):
+    return await Validator.objects.acreate(public_key=validator_key, active=True)
+
+
+@pytest_asyncio.fixture
+async def communicator(validator_key: str):
+    _communicator = WebsocketCommunicator(
         asgi.application, f"v0.1/validator_interface/{validator_key}"
     )
-    connected, _ = await communicator.connect()
+    connected, _ = await _communicator.connect()
     assert connected
+
+    yield _communicator
+
+    await _communicator.disconnect()
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_main_loop(validator_key: str, communicator: WebsocketCommunicator):
+    job_uuid = str(uuid.uuid4())
+    fake_executor.job_uuid = job_uuid
+
     await communicator.send_json_to(
         {
             "message_type": "V0AuthenticateRequest",
@@ -80,4 +99,46 @@ async def test_main_loop():
         "docker_process_stdout": "some stdout",
         "docker_process_stderr": "some stderr",
     }
-    await communicator.disconnect()
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_decline_job(
+    validator_key: str, communicator: WebsocketCommunicator, mocker: MockerFixture
+):
+    mocker.patch(
+        "compute_horde_miner.miner.tests.executor_manager.TestExecutorManager.reserve_executor",
+        side_effect=ExecutorUnavailable,
+    )
+
+    job_uuid = str(uuid.uuid4())
+    fake_executor.job_uuid = job_uuid
+
+    await communicator.send_json_to(
+        {
+            "message_type": "V0AuthenticateRequest",
+            "payload": {
+                "validator_hotkey": validator_key,
+                "miner_hotkey": "some key",
+                "timestamp": int(time.time()),
+            },
+            "signature": "gibberish",
+        }
+    )
+    await communicator.receive_json_from(timeout=WEBSOCKET_TIMEOUT)
+
+    await communicator.send_json_to(
+        {
+            "message_type": "V0InitialJobRequest",
+            "job_uuid": job_uuid,
+            "executor_class": DEFAULT_EXECUTOR_CLASS,
+            "base_docker_image_name": "it's teeeeests",
+            "timeout_seconds": 60,
+            "volume_type": "inline",
+        }
+    )
+    response = await communicator.receive_json_from(timeout=WEBSOCKET_TIMEOUT)
+    assert response == {
+        "message_type": "V0DeclineJobRequest",
+        "job_uuid": job_uuid,
+    }
