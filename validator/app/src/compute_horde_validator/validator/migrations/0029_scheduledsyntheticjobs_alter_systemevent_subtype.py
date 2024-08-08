3436d10cc15b4e474857cb33c12ec113a3f9c35a
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2024-08-08 12:12:23 +0300

    Fix migrations conflict

--- validator/app/src/compute_horde_validator/validator/migrations/0029_scheduledsyntheticjobs_alter_systemevent_subtype.py
+++ validator/app/src/compute_horde_validator/validator/migrations/0029_scheduledsyntheticjobs_alter_systemevent_subtype.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("validator", "0027_weights_weights_unique_block"),
+        ("validator", "0028_alter_systemevent_subtype"),
     ]
 
     operations = [
