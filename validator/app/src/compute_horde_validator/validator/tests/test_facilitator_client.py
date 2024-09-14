c9f6e69cb220af431fc92901a68c709fd3699d47
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-09-14 23:07:06 +0500

    Add test case for machine specs sending in facilitator client

--- validator/app/src/compute_horde_validator/validator/tests/test_facilitator_client.py
+++ validator/app/src/compute_horde_validator/validator/tests/test_facilitator_client.py
@@ -5,14 +5,19 @@ from unittest.mock import patch
 import bittensor
 import pytest
 import websockets
+from channels.layers import get_channel_layer
 
 from compute_horde_validator.validator.models import OrganicJob
+from compute_horde_validator.validator.organic_jobs.facilitator_api import MachineSpecsUpdate
 from compute_horde_validator.validator.organic_jobs.facilitator_client import (
     AuthenticationRequest,
     FacilitatorClient,
     Response,
 )
 from compute_horde_validator.validator.organic_jobs.miner_driver import JobStatusUpdate
+from compute_horde_validator.validator.utils import (
+    MACHINE_SPEC_GROUP_NAME,
+)
 
 from .helpers import (
     MockJobStateMinerClient,
@@ -148,3 +153,65 @@ async def test_facilitator_client(ws_server_cls):
         task.cancel()
         if ws_server.facilitator_error:
             assert False, ws_server.facilitator_error
+
+
+@pytest.fixture
+def specs_msg():
+    return {
+        "type": "machine.specs",
+        "batch_id": str(uuid.uuid4()),
+        "miner_hotkey": "miner_hotkey",
+        "specs": {
+            "cpu": {"cores": 1, "threads": 2, "freq": 3},
+            "gpu": {"name": "gpu_name", "memory": 4, "compute": 5},
+        },
+    }
+
+
+class FacilitatorMachineSpecsWs(FacilitatorWs):
+    async def serve(self, ws, path):
+        response = await asyncio.wait_for(ws.recv(), timeout=5)
+        try:
+            AuthenticationRequest.model_validate_json(response)
+        except Exception as e:
+            self.facilitator_error = e
+
+        await asyncio.wait_for(ws.send(Response(status="success").model_dump_json()), timeout=5)
+
+        async for message in ws:
+            try:
+                MachineSpecsUpdate.model_validate_json(message)
+            except Exception:
+                continue
+            else:
+                async with self.condition:
+                    self.condition.notify()
+
+
+@pytest.mark.asyncio
+@patch("bittensor.subtensor", lambda *args, **kwargs: MockSubtensor())
+@patch("bittensor.metagraph", lambda *args, **kwargs: MockMetagraph())
+@pytest.mark.django_db(databases=["default", "default_alias"], transaction=True)
+async def test_wait_for_specs(specs_msg: dict):
+    layer = get_channel_layer()
+    ws_server = FacilitatorMachineSpecsWs()
+
+    async with websockets.serve(ws_server.serve, "127.0.0.1", 0) as server:
+        host, port = server.sockets[0].getsockname()
+        facilitator_uri = f"ws://{host}:{port}/"
+        facilitator_client = MockFacilitatorClient(get_keypair(), facilitator_uri)
+
+        facilitator_client.MINER_CLIENT_CLASS = MockJobStateMinerClient
+
+        async with ws_server.condition:
+            task = asyncio.create_task(facilitator_client.run_forever())
+            await asyncio.sleep(0.1)
+
+            await layer.group_send(MACHINE_SPEC_GROUP_NAME, specs_msg)
+
+            await asyncio.wait_for(ws_server.condition.wait(), timeout=2)
+
+        facilitator_client.miner_driver_awaiter_task.cancel()
+        facilitator_client.heartbeat_task.cancel()
+        facilitator_client.specs_task.cancel()
+        task.cancel()
