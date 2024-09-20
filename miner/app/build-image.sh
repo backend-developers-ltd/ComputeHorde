a2d2ada2a2540ee1700d2ffca3fe004b450e88bc
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-09-20 17:42:27 +0200

    Enable DCT for staging images; extract DCT key install action

--- miner/app/build-image.sh
+++ miner/app/build-image.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eux -o pipefail
 
-IMAGE_NAME="backenddevelopersltd/compute-horde-miner:v0-latest"
-# Dockerfile assumes that build is run in the parent dir
-cd .. && docker build --platform linux/amd64 --build-context compute-horde=../compute_horde -t $IMAGE_NAME -f app/envs/prod/Dockerfile .
+IMAGE_NAME="${DOCKER_REPOSITORY_NAME:-backenddevelopersltd/compute-horde-miner:v0-latest}"
+
+cd ..
+docker build \
+  --platform linux/amd64 \
+  --build-context compute-horde=../compute_horde \
+  -t "$IMAGE_NAME" \
+  -f app/envs/prod/Dockerfile .
