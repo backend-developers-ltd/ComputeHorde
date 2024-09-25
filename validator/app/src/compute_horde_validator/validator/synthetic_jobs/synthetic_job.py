dbc446508ec5320bdb9c9b74b4b5574397a0bac6
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-09-25 12:14:10 +0200

    Revert testing hashcat job params

--- validator/app/src/compute_horde_validator/validator/synthetic_jobs/synthetic_job.py
+++ validator/app/src/compute_horde_validator/validator/synthetic_jobs/synthetic_job.py
@@ -82,9 +82,9 @@ HASHJOB_PARAMS = {
         Algorithm.SHA512: JobParams(timeout=45, num_letters=5, num_digits=0, num_hashes=1),
     },
     1: {
-        Algorithm.SHA256: JobParams(timeout=153, num_letters=5, num_digits=0, num_hashes=3),
-        Algorithm.SHA384: JobParams(timeout=153, num_letters=5, num_digits=0, num_hashes=3),
-        Algorithm.SHA512: JobParams(timeout=153, num_letters=5, num_digits=0, num_hashes=3),
+        Algorithm.SHA256: JobParams(timeout=53, num_letters=6, num_digits=0, num_hashes=100),
+        Algorithm.SHA384: JobParams(timeout=53, num_letters=5, num_digits=1, num_hashes=100),
+        Algorithm.SHA512: JobParams(timeout=53, num_letters=5, num_digits=1, num_hashes=100),
     },
 }
 HASHJOB_PARAMS[2] = HASHJOB_PARAMS[1]
