8630ce3d774035d0cd4e106a37683ae44abaa530
Enam Mijbah Noor <enam.noor@reef.pl>
2024-10-18 03:03:12 +0600

    Undo move of V0InitialJobRequest

--- compute_horde/compute_horde/mv_protocol/validator_requests.py
+++ compute_horde/compute_horde/mv_protocol/validator_requests.py
@@ -54,6 +54,23 @@ class V0AuthenticateRequest(BaseValidatorRequest):
         return self.payload.blob_for_signing()
 
 
+class V0InitialJobRequest(BaseValidatorRequest, JobMixin):
+    message_type: RequestType = RequestType.V0InitialJobRequest
+    executor_class: ExecutorClass | None = None
+    base_docker_image_name: str | None = None
+    timeout_seconds: int | None = None
+    volume: Volume | None = None
+    volume_type: VolumeType | None = None
+    job_started_receipt_payload: JobStartedReceiptPayload
+    job_started_receipt_signature: str
+
+    @model_validator(mode="after")
+    def validate_volume_or_volume_type(self) -> Self:
+        if bool(self.volume) and bool(self.volume_type):
+            raise ValueError("Expected either `volume` or `volume_type`, got both")
+        return self
+
+
 class V0JobRequest(BaseValidatorRequest, JobMixin):
     message_type: RequestType = RequestType.V0JobRequest
     executor_class: ExecutorClass | None = None
@@ -88,23 +105,6 @@ class V0JobFinishedReceiptRequest(BaseValidatorRequest):
 
     def blob_for_signing(self):
         return self.payload.blob_for_signing()
-
-
-class V0InitialJobRequest(BaseValidatorRequest, JobMixin):
-    message_type: RequestType = RequestType.V0InitialJobRequest
-    executor_class: ExecutorClass | None = None
-    base_docker_image_name: str | None = None
-    timeout_seconds: int | None = None
-    volume: Volume | None = None
-    volume_type: VolumeType | None = None
-    job_started_receipt_payload: JobStartedReceiptPayload
-    job_started_receipt_signature: str
-
-    @model_validator(mode="after")
-    def validate_volume_or_volume_type(self) -> Self:
-        if bool(self.volume) and bool(self.volume_type):
-            raise ValueError("Expected either `volume` or `volume_type`, got both")
-        return self
 
 
 class V0JobAcceptedReceiptRequest(BaseValidatorRequest):
