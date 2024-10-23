5d70f0aaca421ec8090a094a2d4ee80eb81603c7
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-10-23 22:10:17 +0300

    added fv_protocol package

---
+++ compute_horde/compute_horde/fv_protocol/validator_requests.py
@@ -0,0 +1,35 @@
+from typing import Any, Self
+
+import bittensor
+from pydantic import BaseModel
+
+
+class Heartbeat(BaseModel, extra="forbid"):
+    """Message sent from validator to facilitator to keep connection alive"""
+
+    message_type: str = "V0Heartbeat"
+
+
+class AuthenticationRequest(BaseModel, extra="forbid"):
+    """Message sent from validator to facilitator to authenticate itself"""
+
+    message_type: str = "V0AuthenticationRequest"
+    public_key: str
+    signature: str
+
+    @classmethod
+    def from_keypair(cls, keypair: bittensor.Keypair) -> Self:
+        return cls(
+            public_key=keypair.public_key.hex(),
+            signature=f"0x{keypair.sign(keypair.public_key).hex()}",
+        )
+
+
+class MachineSpecsUpdate(BaseModel, extra="forbid"):
+    """Message sent from validator to facilitator to update miner specs"""
+
+    message_type: str = "V0MachineSpecsUpdate"
+    miner_hotkey: str
+    validator_hotkey: str
+    specs: dict[str, Any]
+    batch_id: str
