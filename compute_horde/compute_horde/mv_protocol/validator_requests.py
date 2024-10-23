632825f5b59001a8b01f85c27acf1783f7bebe16
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-10-23 14:31:54 +0200

    default value on is_organic is not needed

--- compute_horde/compute_horde/mv_protocol/validator_requests.py
+++ compute_horde/compute_horde/mv_protocol/validator_requests.py
@@ -133,7 +133,7 @@ class JobStartedReceiptPayload(ReceiptPayload):
     executor_class: ExecutorClass
     time_accepted: datetime.datetime
     max_timeout: int  # seconds
-    is_organic: bool = False
+    is_organic: bool
 
     @field_serializer("time_accepted")
     def serialize_dt(self, dt: datetime.datetime, _info):
