153987231e2526824325776b2756fd254a8aea47
Enam Mijbah Noor <enam.noor@reef.pl>
2024-10-23 16:04:29 +0600

    Fix remaining miner test

--- miner/app/src/compute_horde_miner/miner/tests/integration/test_mocked_executor_manager.py
+++ miner/app/src/compute_horde_miner/miner/tests/integration/test_mocked_executor_manager.py
@@ -1,12 +1,10 @@
 import contextlib
-import json
 import time
 import uuid
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 
 import pytest
 import pytest_asyncio
-from bittensor import Keypair
 from channels.testing import WebsocketCommunicator
 from compute_horde.executor_class import DEFAULT_EXECUTOR_CLASS
 from pytest_mock import MockerFixture
@@ -22,11 +20,9 @@ WEBSOCKET_TIMEOUT = 3
 
 @pytest.fixture
 def mock_keypair(mocker: MockerFixture):
-    mock = Mock(wraps=Keypair)
-    mocker.patch(
-        "compute_horde_miner.miner.miner_consumer.validator_interface.bittensor.Keypair", mock
+    return mocker.patch(
+        "compute_horde_miner.miner.miner_consumer.validator_interface.bittensor.Keypair"
     )
-    return mock
 
 
 # Somehow the regular dependency mechanism doesn't work with multiple test cases
@@ -48,15 +44,8 @@ def job_uuid():
 
 
 @pytest.fixture
-def validator_keypair():
-    return Keypair.create_from_mnemonic(
-        "slot excuse valid grief praise rifle spoil auction weasel glove pen share"
-    )
-
-
-@pytest.fixture
-def validator_hotkey(validator_keypair):
-    return validator_keypair.ss58_address
+def validator_hotkey():
+    return "some_public_key"
 
 
 @pytest_asyncio.fixture
@@ -75,20 +64,17 @@ async def make_communicator(validator_key: str):
     await communicator.disconnect()
 
 
-async def run_regular_flow_test(validator_keypair: Keypair, miner_hotkey: str, job_uuid: str):
-    async with make_communicator(validator_keypair.ss58_address) as communicator:
-        auth_payload = {
-            "validator_hotkey": validator_keypair.ss58_address,
-            "miner_hotkey": "some key",
-            "timestamp": int(time.time()),
-        }
-        auth_payload_blob = json.dumps(auth_payload, sort_keys=True)
-        auth_signature = f"0x{validator_keypair.sign(auth_payload_blob).hex()}"
+async def run_regular_flow_test(validator_key: str, job_uuid: str):
+    async with make_communicator(validator_key) as communicator:
         await communicator.send_json_to(
             {
                 "message_type": "V0AuthenticateRequest",
-                "payload": auth_payload,
-                "signature": auth_signature,
+                "payload": {
+                    "validator_hotkey": validator_key,
+                    "miner_hotkey": "some key",
+                    "timestamp": int(time.time()),
+                },
+                "signature": "gibberish",
             }
         )
         response = await communicator.receive_json_from(timeout=WEBSOCKET_TIMEOUT)
@@ -98,18 +84,6 @@ async def run_regular_flow_test(validator_keypair: Keypair, miner_hotkey: str, j
                 "executor_classes": [{"count": 1, "executor_class": DEFAULT_EXECUTOR_CLASS}]
             },
         }
-        receipt = {
-            "receipt_type": "JobStartedReceipt",
-            "job_uuid": job_uuid,
-            "miner_hotkey": miner_hotkey,
-            "validator_hotkey": validator_keypair.ss58_address,
-            "timestamp": "2020-01-01T00:00Z",
-            "executor_class": DEFAULT_EXECUTOR_CLASS,
-            "max_timeout": 60,
-            "ttl": 5,
-        }
-        receipt_blob = json.dumps(receipt, sort_keys=True)
-        receipt_signature = f"0x{validator_keypair.sign(receipt_blob).hex()}"
         await communicator.send_json_to(
             {
                 "message_type": "V0InitialJobRequest",
@@ -118,8 +92,17 @@ async def run_regular_flow_test(validator_keypair: Keypair, miner_hotkey: str, j
                 "base_docker_image_name": "it's teeeeests",
                 "timeout_seconds": 60,
                 "volume_type": "inline",
-                "job_started_receipt_payload": receipt,
-                "job_started_receipt_signature": receipt_signature,
+                "job_started_receipt_payload": {
+                    "receipt_type": "JobStartedReceipt",
+                    "job_uuid": job_uuid,
+                    "miner_hotkey": "miner_hotkey",
+                    "validator_hotkey": validator_key,
+                    "timestamp": "2020-01-01T00:00Z",
+                    "executor_class": DEFAULT_EXECUTOR_CLASS,
+                    "max_timeout": 60,
+                    "ttl": 5,
+                },
+                "job_started_receipt_signature": "gibberish",
             }
         )
         response = await communicator.receive_json_from(timeout=WEBSOCKET_TIMEOUT)
@@ -153,32 +136,21 @@ async def run_regular_flow_test(validator_keypair: Keypair, miner_hotkey: str, j
         }
 
 
-async def test_main_loop(validator: Validator, validator_keypair: Keypair, job_uuid: str, settings):
-    await run_regular_flow_test(
-        validator_keypair,
-        settings.BITTENSOR_WALLET().hotkey.ss58_address,
-        job_uuid,
-    )
+async def test_main_loop(validator: Validator, job_uuid: str, mock_keypair: MagicMock):
+    await run_regular_flow_test(validator.public_key, job_uuid)
 
 
-async def test_local_miner(
-    validator: Validator,
-    validator_keypair: Keypair,
-    job_uuid: str,
-    mock_keypair: Mock,
-    settings,
-):
+async def test_local_miner(validator: Validator, job_uuid: str, mock_keypair: MagicMock, settings):
     settings.IS_LOCAL_MINER = True
     settings.DEBUG_TURN_AUTHENTICATION_OFF = False
 
-    await run_regular_flow_test(
-        validator_keypair, settings.BITTENSOR_WALLET().hotkey.ss58_address, job_uuid
-    )
+    await run_regular_flow_test(validator.public_key, job_uuid)
 
     mock_keypair.assert_called_once_with(ss58_address=validator.public_key)
+    mock_keypair.return_value.verify.assert_called_once()
 
 
-async def test_local_miner_unknown_validator(mock_keypair: Mock, settings):
+async def test_local_miner_unknown_validator(mock_keypair: MagicMock, settings):
     settings.IS_LOCAL_MINER = True
     settings.DEBUG_TURN_AUTHENTICATION_OFF = False
 
