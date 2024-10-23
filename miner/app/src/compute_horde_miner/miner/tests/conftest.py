208013cb0407393ee424ecbb647e3c820dc9efe9
Enam Mijbah Noor <enam.noor@reef.pl>
2024-10-23 16:43:11 +0600

    Add fixture to create miner test wallet

--- miner/app/src/compute_horde_miner/miner/tests/conftest.py
+++ miner/app/src/compute_horde_miner/miner/tests/conftest.py
@@ -1,6 +1,10 @@
+import logging
 from collections.abc import Generator
 
+import bittensor
 import pytest
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -8,3 +12,14 @@ def some() -> Generator[int, None, None]:
     # setup code
     yield 1
     # teardown code
+
+
+@pytest.fixture(scope="session", autouse=True)
+def wallet():
+    wallet = bittensor.wallet(name="test_miner")
+    try:
+        # workaround the overwrite flag
+        wallet.regenerate_coldkey(seed="0" * 64, use_password=False, overwrite=True)
+        wallet.regenerate_hotkey(seed="1" * 64, use_password=False, overwrite=True)
+    except Exception as e:
+        logger.error(f"Failed to create wallet: {e}")
