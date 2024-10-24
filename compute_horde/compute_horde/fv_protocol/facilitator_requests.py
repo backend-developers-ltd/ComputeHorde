b7f01baf7bc0d75a0529968031225161bc1669ad
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-10-24 10:39:28 +0300

    added V2JobRequest

--- compute_horde/compute_horde/fv_protocol/facilitator_requests.py
+++ compute_horde/compute_horde/fv_protocol/facilitator_requests.py
@@ -1,7 +1,7 @@
 from typing import Annotated, Literal, Self
 
 import pydantic
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, JsonValue, model_validator
 
 from compute_horde.base.output_upload import OutputUpload, ZipAndHttpPutUpload
 from compute_horde.base.volume import Volume, ZipUrlVolume
@@ -19,6 +19,14 @@ class Response(BaseModel, extra="forbid"):
 
     status: Literal["error", "success"]
     errors: list[Error] = []
+
+
+class SignedRequest(BaseModel, extra="forbid"):
+    signature_type: str
+    signatory: str
+    timestamp_ns: int
+    signature: str
+    signed_payload: JsonValue
 
 
 class V0JobRequest(BaseModel, extra="forbid"):
@@ -77,6 +85,34 @@ class V1JobRequest(BaseModel, extra="forbid"):
     use_gpu: bool
     volume: Volume | None = None
     output_upload: OutputUpload | None = None
+
+    def get_args(self):
+        return self.args
+
+    @model_validator(mode="after")
+    def validate_at_least_docker_image_or_raw_script(self) -> Self:
+        if not (bool(self.docker_image) or bool(self.raw_script)):
+            raise ValueError("Expected at least one of `docker_image` or `raw_script`")
+        return self
+
+
+class V2JobRequest(BaseModel, extra="forbid"):
+    """Message sent from facilitator to validator to request a job execution"""
+
+    # this points to a `ValidatorConsumer.job_new` handler (fuck you django-channels!)
+    type: Literal["job.new"] = "job.new"
+    message_type: Literal["V2JobRequest"] = "V2JobRequest"
+    uuid: str
+    miner_hotkey: str | None
+    executor_class: ExecutorClass
+    docker_image: str
+    raw_script: str
+    args: list[str]
+    env: dict[str, str]
+    use_gpu: bool
+    volume: Volume | None = None
+    output_upload: OutputUpload | None = None
+    signed_request: SignedRequest
 
     def get_args(self):
         return self.args
