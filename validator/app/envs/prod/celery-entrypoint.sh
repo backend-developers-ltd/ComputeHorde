04e4d35e8e3b9f4b57e99b0771af68df93bb67cd
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-10-29 15:04:06 +0100

    move celery commandline to compose

--- validator/app/envs/prod/celery-entrypoint.sh
+++ validator/app/envs/prod/celery-entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 set -eu
 
-if [ -z "${CELERY_QUEUE:-}" ]; then
-    echo "Error: you must set the CELERY_QUEUE variable to the name of the queue for this worker."
-    exit 1
-fi
-
-# Default loglevel = INFO
-CELERY_LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
-
 ./prometheus-cleanup.sh
 ./manage.py wait_for_database --timeout 10
 
-celery -A compute_horde_validator worker -Q "$CELERY_QUEUE" -E -l "$CELERY_LOGLEVEL" -c "$CELERY_CONCURRENCY"
+celery -A compute_horde_validator worker "$@"
