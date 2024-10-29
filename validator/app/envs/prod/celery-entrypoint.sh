c5faf46342ddc92e5d7a79a9c59a4d3008a2fb2b
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-10-29 14:41:08 +0100

    remove weird warning

--- validator/app/envs/prod/celery-entrypoint.sh
+++ validator/app/envs/prod/celery-entrypoint.sh
@@ -9,8 +9,7 @@ fi
 # Default loglevel = INFO
 CELERY_LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
 
-type prometheus-cleanup.sh && ./prometheus-cleanup.sh
-
+./prometheus-cleanup.sh
 ./manage.py wait_for_database --timeout 10
 
 celery -A compute_horde_validator worker -Q $CELERY_QUEUE -E -l $CELERY_LOGLEVEL -c $CELERY_CONCURRENCY
