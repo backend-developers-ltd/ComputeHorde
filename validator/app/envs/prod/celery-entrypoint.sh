a6fd87a5e81814edb9d5b308dd8fc514966dedfc
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-10-29 01:20:54 +0100

    default celery concurrency = 2

--- validator/app/envs/prod/celery-entrypoint.sh
+++ validator/app/envs/prod/celery-entrypoint.sh
@@ -7,10 +7,10 @@ if [ -z "${CELERY_QUEUE:-}" ]; then
 fi
 
 # Default loglevel = INFO
-: "${CELERY_LOGLEVEL:=INFO}"
+CELERY_LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
 
 type prometheus-cleanup.sh && ./prometheus-cleanup.sh
 
 ./manage.py wait_for_database --timeout 10
 
-celery -A compute_horde_validator worker -Q $CELERY_QUEUE -E -l $CELERY_LOGLEVEL
+celery -A compute_horde_validator worker -Q $CELERY_QUEUE -E -l $CELERY_LOGLEVEL -c $CELERY_CONCURRENCY
