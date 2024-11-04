42974a0d3491370a625fe13d8c8d54e3656bbc05
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-11-04 16:30:10 +0100

    max celery tasks per child process with default=10

--- validator/app/envs/prod/celery-entrypoint.sh
+++ validator/app/envs/prod/celery-entrypoint.sh
@@ -3,8 +3,14 @@ set -eu
 
 CELERY_CONCURRENCY=${CELERY_CONCURRENCY:-2}
 CELERY_LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
+CELERY_MAX_TASKS_PER_CHILD=${CELERY_MAX_TASKS_PER_CHILD:-10}
 
 ./prometheus-cleanup.sh
 ./manage.py wait_for_database --timeout 10
 
-celery -A compute_horde_validator worker -E -l "$CELERY_LOGLEVEL" -c "${CELERY_CONCURRENCY}" "$@"
+celery -A compute_horde_validator worker \
+  -E \
+  -l "${CELERY_LOGLEVEL}" \
+  -c "${CELERY_CONCURRENCY}" \
+  --max-tasks-per-child "${CELERY_MAX_TASKS_PER_CHILD}" \
+  "$@"
