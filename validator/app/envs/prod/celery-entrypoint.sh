98ef37d6a7aacb126f0c02eb09e62119de314006
Kordian Kowalski <kordian.kowalski@reef.pl>
2024-11-04 16:15:50 +0100

    Move common celery args to entrypoint; use env vars to optionally override the defaults; fix yaml include env override losing `debug=off`;

--- validator/app/envs/prod/celery-entrypoint.sh
+++ validator/app/envs/prod/celery-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -eu
 
+CELERY_CONCURRENCY=${CELERY_CONCURRENCY:-2}
+CELERY_LOGLEVEL=${CELERY_LOGLEVEL:-INFO}
+
 ./prometheus-cleanup.sh
 ./manage.py wait_for_database --timeout 10
 
-celery -A compute_horde_validator worker "$@"
+celery -A compute_horde_validator worker -E -l "$CELERY_LOGLEVEL" -c "${CELERY_CONCURRENCY}" "$@"
