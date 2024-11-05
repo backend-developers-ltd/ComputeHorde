805d5de7fd66b36acd6f03e2bf2ee9b6a92ccbb9
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-11-06 00:52:14 +0500

    Replace pdm with uv in CI

--- compute_horde/noxfile.py
+++ compute_horde/noxfile.py
@@ -17,12 +17,14 @@ def install(session: nox.Session, *args):
     groups = []
     for group in args:
         groups.extend(["--group", group])
+
+    uv_env = getattr(session.virtualenv, "location", os.getenv("VIRTUAL_ENV"))
     session.run_install(
         "uv",
         "sync",
         "--locked",
         *groups,
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+        env={"UV_PROJECT_ENVIRONMENT": uv_env},
     )
 
 
