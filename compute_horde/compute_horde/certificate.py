12bb4998e00046e4207151f702c01a35211b5204
Enam Mijbah Noor <enam.noor@reef.pl>
2024-11-19 04:47:47 +0600

    Remove CA from certificate generation

--- compute_horde/compute_horde/certificate.py
+++ compute_horde/compute_horde/certificate.py
@@ -9,99 +9,36 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from cryptography.x509 import Certificate
 from cryptography.x509.oid import NameOID
 
-CA_COMMON_NAME = "Custom CA"
-COMMON_NAME = "dummy.computehorde.io"
-COUNTRY_NAME = "US"
-ORGANIZATION_NAME = "ComputeHorde"
 
-
-def generate_certificate_authority() -> tuple[Certificate, RSAPrivateKey]:
-    # Generate CA private key
+def generate_certificate(alternative_name: str) -> tuple[Certificate, RSAPrivateKey]:
+    """Generate a certificate and private key"""
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
-    # Generate CA certificate
     builder = x509.CertificateBuilder()
     builder = builder.subject_name(
-        x509.Name(
-            [
-                x509.NameAttribute(NameOID.COMMON_NAME, CA_COMMON_NAME),
-                x509.NameAttribute(NameOID.ORGANIZATION_NAME, ORGANIZATION_NAME),
-                x509.NameAttribute(NameOID.COUNTRY_NAME, COUNTRY_NAME),
-            ]
-        )
+        x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "localhost")])
     )
-    builder = builder.issuer_name(
-        x509.Name(
-            [
-                x509.NameAttribute(NameOID.COMMON_NAME, CA_COMMON_NAME),
-                x509.NameAttribute(NameOID.ORGANIZATION_NAME, ORGANIZATION_NAME),
-                x509.NameAttribute(NameOID.COUNTRY_NAME, COUNTRY_NAME),
-            ]
-        )
-    )
+    builder = builder.issuer_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, "localhost")]))
     builder = builder.not_valid_before(datetime.now(UTC))
     builder = builder.not_valid_after(datetime.now(UTC) + timedelta(days=365))
     builder = builder.serial_number(x509.random_serial_number())
     builder = builder.public_key(private_key.public_key())
-    builder = builder.add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True)
-
-    # Sign CA certificate with its own private key
-    certificate = builder.sign(private_key=private_key, algorithm=hashes.SHA256())
-
-    return certificate, private_key
-
-
-def generate_certificate(
-    certificate_authority: tuple[Certificate, RSAPrivateKey] | None = None,
-    public_ip: str | None = None,
-) -> tuple[Certificate, RSAPrivateKey]:
-    """
-    Generate a certificate and public key.
-
-    :param certificate_authority: if given, the CA will be used to generate the new certificate.
-    :param public_ip: if given, adds the ip as an alternative name to the certificate
-    :return:
-    """
-    if certificate_authority is None:
-        ca_cert, ca_key = generate_certificate_authority()
-    else:
-        ca_cert, ca_key = certificate_authority
-
-    # Generate private key
-    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-
-    # Generate certificate
-    builder = x509.CertificateBuilder()
-    builder = builder.subject_name(
-        x509.Name(
-            [
-                x509.NameAttribute(NameOID.COMMON_NAME, COMMON_NAME),
-                x509.NameAttribute(NameOID.ORGANIZATION_NAME, ORGANIZATION_NAME),
-                x509.NameAttribute(NameOID.COUNTRY_NAME, COUNTRY_NAME),
-            ]
-        )
-    )
-    builder = builder.issuer_name(ca_cert.subject)
-    builder = builder.not_valid_before(datetime.now(UTC))
-    builder = builder.not_valid_after(datetime.now(UTC) + timedelta(days=365))
-    builder = builder.serial_number(x509.random_serial_number())
-    builder = builder.public_key(private_key.public_key())
-
-    # Add extensions
     builder = builder.add_extension(
         x509.BasicConstraints(ca=False, path_length=None), critical=True
     )
-    alternative_names = [
-        x509.DNSName(COMMON_NAME),
-        x509.DNSName("localhost"),
-        x509.IPAddress(ipaddress.ip_address("127.0.0.1")),
-    ]
-    if public_ip:
-        alternative_names.append(x509.IPAddress(ipaddress.ip_address(public_ip)))
-    builder = builder.add_extension(x509.SubjectAlternativeName(alternative_names), critical=False)
 
-    # Sign certificate with CA private key
-    certificate = builder.sign(private_key=ca_key, algorithm=hashes.SHA256())
+    try:
+        _ip = ipaddress.ip_address(alternative_name)
+    except ValueError:
+        alt_name = x509.DNSName(alternative_name)
+    else:
+        alt_name = x509.IPAddress(_ip)
+
+    builder = builder.add_extension(
+        x509.SubjectAlternativeName([x509.DNSName("localhost"), alt_name]), critical=False
+    )
+
+    certificate = builder.sign(private_key=private_key, algorithm=hashes.SHA256())
 
     return certificate, private_key
 
@@ -126,26 +63,10 @@ def write_private_key(private_key: RSAPrivateKey, path: Path) -> None:
     path.write_bytes(serialize_private_key(private_key))
 
 
-def generate_ca_and_server_certs(dir_path: Path, public_ip: str | None = None) -> None:
-    """
-    Generates certificate authority and server certificates.
-
-    :param dir_path: directory where the generated certificates and public keys will be saved
-    :param public_ip: if given, adds the ip as an alternative name to the server certificate
-    :return:
-    """
+def generate_certificate_at(dir_path: Path, alternative_name: str) -> None:
+    """Generates and saves a certificate and private key at `dir_path`"""
     assert dir_path.is_dir()
 
-    ca_cert, ca_key = generate_certificate_authority()
-
-    server_cert, server_key = generate_certificate(
-        certificate_authority=(ca_cert, ca_key),
-        public_ip=public_ip,
-    )
-
-    # Save the certificates and keys
-    write_certificate(ca_cert, dir_path / "ca.crt")
-    write_private_key(ca_key, dir_path / "ca.key")
-
-    write_certificate(server_cert, dir_path / "server.crt")
-    write_private_key(server_key, dir_path / "server.key")
+    certificate, private_key = generate_certificate(alternative_name)
+    write_certificate(certificate, dir_path / "certificate.pem")
+    write_private_key(private_key, dir_path / "private_key.pem")
--- compute_horde/tests/test_certificate.py
+++ compute_horde/tests/test_certificate.py
@@ -4,23 +4,17 @@ from cryptography import x509
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 
-from compute_horde.certificate import generate_ca_and_server_certs
+from compute_horde.certificate import generate_certificate_at
 
 
 def test_generate_ca_and_server_certs(tmp_path):
-    generate_ca_and_server_certs(tmp_path)
+    generate_certificate_at(tmp_path, "blabla")
 
-    cert_files = ["ca.crt", "server.crt"]
-    key_files = ["ca.key", "server.key"]
-    expected_files = cert_files + key_files
+    assert set(os.listdir(tmp_path)) == {"certificate.pem", "private_key.pem"}
 
-    assert set(os.listdir(tmp_path)) == set(expected_files)
+    # check the cert is valid
+    x509.load_pem_x509_certificate((tmp_path / "certificate.pem").read_bytes())
 
-    # check certs are valid
-    for cert_file in cert_files:
-        x509.load_pem_x509_certificate((tmp_path / cert_file).read_bytes())
-
-    # check public keys are valid
-    for key_file in key_files:
-        key = serialization.load_pem_private_key((tmp_path / key_file).read_bytes(), None)
-        assert isinstance(key, RSAPrivateKey)
+    # check the public key is valid
+    key = serialization.load_pem_private_key((tmp_path / "private_key.pem").read_bytes(), None)
+    assert isinstance(key, RSAPrivateKey)
