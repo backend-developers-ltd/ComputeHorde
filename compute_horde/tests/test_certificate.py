237d9184aa876f9189d648dad45c7acaeaf00575
Andreea Popescu <andreea.popescu@reef.pl>
2024-11-23 07:19:07 +0000

    nginx verify certificate

--- compute_horde/tests/test_certificate.py
+++ compute_horde/tests/test_certificate.py
@@ -1,7 +1,5 @@
 import os
 import subprocess
-import time
-import uuid
 
 import pytest
 import requests
@@ -12,8 +10,34 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from compute_horde.certificate import (
     generate_certificate,
     generate_certificate_at,
+    start_nginx_with_certificates,
     write_certificate,
 )
+
+NGINX_CONF = """
+http {
+    server {
+        listen 443 ssl;
+        server_name example.com;
+
+        ssl_certificate /etc/nginx/ssl/certificate.pem;
+        ssl_certificate_key /etc/nginx/ssl/private_key.pem;
+
+        ssl_client_certificate /etc/nginx/ssl/client.crt;
+        ssl_verify_client on;
+
+        location / {
+            if ($ssl_client_verify != SUCCESS) { return 403; }
+            return 200 'Hello World!';
+            add_header Content-Type text/plain;
+        }
+    }
+}
+
+events {
+    worker_connections 1024;
+}
+"""
 
 
 def test_generate_ca_and_server_certs(tmp_path):
@@ -29,74 +53,53 @@ def test_generate_ca_and_server_certs(tmp_path):
     assert isinstance(key, RSAPrivateKey)
 
 
-NGINX_CONF = """
-http {
-    server {
-        listen 443 ssl;
-        server_name example.com;
+def make_cert(tmp_path):
+    client_tmp_path = tmp_path / "client"
+    client_tmp_path.mkdir()
 
-        ssl_certificate /etc/nginx/ssl/certificate.pem;
-        ssl_certificate_key /etc/nginx/ssl/private_key.pem;
+    generate_certificate_at(client_tmp_path, "blabla")
+    public_key = (client_tmp_path / "certificate.pem").read_bytes()
 
-        location / {
-            return 200 'Hello World!';
-            add_header Content-Type text/plain;
-        }
-    }
-}
-
-events {
-    worker_connections 1024;
-}
-"""
-
-
-def certificates_with_nginx_helper(tmp_path, docker_name_prefix):
-    certs_dir = tmp_path / "ssl"
-    certs_dir.mkdir()
-    generate_certificate_at(certs_dir, "127.0.0.1")
-    nginx_conf_file = tmp_path / "nginx.conf"
-    nginx_conf_file.write_text(NGINX_CONF)
-
-    container_name = f"{docker_name_prefix}_{uuid.uuid4()}"
-    subprocess.run(
-        [
-            "docker",
-            "run",
-            "--detach",
-            "--rm",
-            "--name",
-            container_name,
-            "-p",
-            "8080:80",
-            "-p",
-            "8443:443",
-            "-v",
-            f"{nginx_conf_file}:/etc/nginx/nginx.conf",
-            "-v",
-            f"{certs_dir}:/etc/nginx/ssl",
-            "nginx:1.26-alpine",
-        ]
+    return public_key, (
+        str(client_tmp_path / "certificate.pem"),
+        str(client_tmp_path / "private_key.pem"),
     )
 
-    # wait for nginx to start
-    time.sleep(1)
 
-    return container_name, certs_dir
+def test_certificates_with_nginx__success(tmp_path, docker_name_prefix):
+    public_key, cert = make_cert(tmp_path)
+    container_name, certs_dir = start_nginx_with_certificates(
+        NGINX_CONF, public_key, docker_name_prefix, tmp_path
+    )
 
-
-def test_certificates_with_nginx_success(tmp_path, docker_name_prefix):
-    container_name, certs_dir = certificates_with_nginx_helper(tmp_path, docker_name_prefix)
-
-    resp = requests.get("https://localhost:8443", verify=str(certs_dir / "certificate.pem"))
+    resp = requests.get(
+        "https://localhost:8443", verify=str(certs_dir / "certificate.pem"), cert=cert
+    )
     assert resp.status_code == 200
     assert resp.text.strip() == "Hello World!"
 
     subprocess.run(["docker", "kill", container_name])
 
 
-def test_certificates_with_nginx_fail(tmp_path, docker_name_prefix):
-    container_name, _ = certificates_with_nginx_helper(tmp_path, docker_name_prefix)
+def test_certificates_with_nginx__no_cert(tmp_path, docker_name_prefix):
+    public_key, _ = make_cert(tmp_path)
+    container_name, certs_dir = start_nginx_with_certificates(
+        NGINX_CONF, public_key, docker_name_prefix, tmp_path
+    )
+
+    resp = requests.get("https://localhost:8443", verify=str(certs_dir / "certificate.pem"))
+
+    # 400 no cert, 403 wrong cert
+    assert resp.status_code != 200
+
+    subprocess.run(["docker", "kill", container_name])
+
+
+def test_certificates_with_nginx__fail(tmp_path, docker_name_prefix):
+    public_key, _ = make_cert(tmp_path)
+    container_name, _ = start_nginx_with_certificates(
+        NGINX_CONF, public_key, docker_name_prefix, tmp_path
+    )
 
     cert_path = tmp_path / "wrong_certificate.pem"
     certificate, _ = generate_certificate("127.0.0.1")
