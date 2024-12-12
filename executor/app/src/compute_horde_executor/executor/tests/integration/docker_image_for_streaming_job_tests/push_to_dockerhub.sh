6f4f3a781ed25ef17c05e14579ff327e2f31b46d
Andreea Popescu <andreea.popescu@reef.pl>
2024-12-12 07:49:46 +0000

    executor streaming job test

---
+++ executor/app/src/compute_horde_executor/executor/tests/integration/docker_image_for_streaming_job_tests/push_to_dockerhub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# Define image
+IMAGE_NAME="backenddevelopersltd/compute-horde-streaming-job-test:v0-latest"
+
+# Build the Docker image
+docker build -t $IMAGE_NAME .
+
+# Login to GitHub Docker Registry
+echo "$DOCKERHUB_PAT" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
+# Push image to GitHub Docker Registry
+docker push $IMAGE_NAME
