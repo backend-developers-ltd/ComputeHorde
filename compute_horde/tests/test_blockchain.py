ec1e6079cad6aba871762702a1d03718cc5f05dd
Sławomir Górawski <slawomir.gorawski@reef.pl>
2024-12-19 12:40:17 +0100

    wip

--- compute_horde/tests/test_blockchain.py
+++ compute_horde/tests/test_blockchain.py
@@ -1,14 +1,39 @@
+from unittest.mock import patch
+
+from django.core.cache import cache
+from django.conf import settings
+
+from compute_horde.blockchain.block_cache import get_current_block
+from compute_horde.blockchain.tasks import update_block_cache
+
+
 def test_block_cache_get_current_block_in_cache():
-    pass
+    cache.set(settings.COMPUTE_HORDE_BLOCK_CACHE_KEY, 123)
+
+    with patch('bittensor.subtensor') as mock_subtensor:
+        current_block = get_current_block()
+
+        assert current_block == 123
+        mock_subtensor.assert_not_called()
 
 
 def test_block_cache_get_current_block_not_in_cache():
-    pass
+    with patch('bittensor.subtensor') as mock_subtensor:
+        mock_subtensor.block = 123
 
+        current_block = get_current_block()
 
-def test_block_cache_cache_current_block():
-    pass
+        assert current_block == 123
 
 
 def test_tasks_update_block_cache():
-    pass
+    with patch('bittensor.subtensor') as mock_subtensor:
+        mock_subtensor.block = 123
+
+        update_block_cache.delay()
+
+        current_block = get_current_block()
+
+        assert current_block == 123
+
+        mock_subtensor.assert_called_once()
