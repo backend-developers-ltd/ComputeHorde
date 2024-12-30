ff1fe08052568ac98524ceec6912177280edb37c
Sławomir Górawski <slawomir.gorawski@reef.pl>
2024-12-30 12:35:07 +0100

    Apply code review feedback

--- compute_horde/compute_horde/blockchain/block_cache.py
+++ compute_horde/compute_horde/blockchain/block_cache.py
@@ -1,4 +1,8 @@
+import asyncio
+import functools
+
 import bittensor
+from asgiref.sync import async_to_sync
 from django.conf import settings
 from django.core.cache import cache
 
@@ -8,22 +12,40 @@ _BLOCK_CACHE_KEY = getattr(
 _BLOCK_CACHE_TIMEOUT = getattr(settings, "COMPUTE_HORDE_BLOCK_CACHE_TIMEOUT", 2)
 
 
-def get_subtensor(network):
+class BlockNotInCacheError(KeyError):
+    pass
+
+
+@functools.cache
+def _get_subtensor(network):
     return bittensor.subtensor(network)
 
 
-def get_current_block() -> int:
+async def aget_current_block(timeout: float = 1.0) -> int:
+    """
+    Gets the current block number from cache. Waits for ``timeout`` seconds if it's not there.
+
+    :param timeout: Number of seconds to wait for the block. Pass 0 to return or raise an exception immediately instead.
+    :raise BlockNotInCacheError: If block was not found in cache.
+    """
+    # Note: cache.get here is not async.
     current_block: int = cache.get(_BLOCK_CACHE_KEY)
     if current_block is not None:
         return current_block
 
-    return cache_current_block()
+    if timeout <= 0:
+        raise BlockNotInCacheError(_BLOCK_CACHE_KEY)
+
+    await asyncio.sleep(timeout)
+
+    return await aget_current_block(timeout=0)
 
 
-def cache_current_block() -> int:
-    subtensor = get_subtensor(network=settings.BITTENSOR_NETWORK)
-    current_block: int = subtensor.get_current_block()
+get_current_block = async_to_sync(aget_current_block)
+
+
+def cache_current_block() -> None:
+    subtensor = _get_subtensor(network=settings.BITTENSOR_NETWORK)
+    current_block = subtensor.get_current_block()
 
     cache.set(_BLOCK_CACHE_KEY, current_block, _BLOCK_CACHE_TIMEOUT)
-
-    return current_block
--- compute_horde/tests/test_blockchain.py
+++ compute_horde/tests/test_blockchain.py
@@ -1,3 +1,4 @@
+import asyncio
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -5,7 +6,11 @@ from celery import Celery
 from django.conf import settings
 from django.core.cache import cache
 
-from compute_horde.blockchain.block_cache import get_current_block
+from compute_horde.blockchain.block_cache import (
+    BlockNotInCacheError,
+    aget_current_block,
+    get_current_block,
+)
 from compute_horde.blockchain.tasks import update_block_cache
 
 
@@ -23,27 +28,45 @@ def celery():
     app.close()
 
 
+@pytest.mark.asyncio
+async def test_block_cache_aget_current_block_in_cache():
+    cache.set(settings.COMPUTE_HORDE_BLOCK_CACHE_KEY, 123)
+
+    current_block = await aget_current_block()
+
+    assert current_block == 123
+
+
+@pytest.mark.asyncio
+async def test_block_cache_aget_current_block_wait():
+    async def populate_after_delay():
+        await asyncio.sleep(0.1)
+        cache.set(settings.COMPUTE_HORDE_BLOCK_CACHE_KEY, 123)
+
+    asyncio.create_task(populate_after_delay())
+
+    current_block = await aget_current_block()
+
+    assert current_block == 123
+
+
+@pytest.mark.asyncio
+async def test_block_cache_aget_current_block_timeout():
+    with pytest.raises(BlockNotInCacheError):
+        await aget_current_block(timeout=0.01)
+
+
 def test_block_cache_get_current_block_in_cache():
     cache.set(settings.COMPUTE_HORDE_BLOCK_CACHE_KEY, 123)
 
-    mock_subtensor = MagicMock()
-    with patch("bittensor.subtensor", return_value=mock_subtensor):
-        mock_subtensor.get_current_block.return_value = 123
+    current_block = get_current_block()
 
-        current_block = get_current_block()
-
-        assert current_block == 123
-        mock_subtensor.get_current_block.assert_not_called()
+    assert current_block == 123
 
 
-def test_block_cache_get_current_block_not_in_cache():
-    mock_subtensor = MagicMock()
-    with patch("bittensor.subtensor", return_value=mock_subtensor):
-        mock_subtensor.get_current_block.return_value = 123
-
-        current_block = get_current_block()
-
-        assert current_block == 123
+def test_block_cache_get_current_block_timeout():
+    with pytest.raises(BlockNotInCacheError):
+        get_current_block()
 
 
 def test_tasks_update_block_cache(celery):
