c10166738d05a02535760950585c8437ccca6fcb
Andreea Popescu <andreea.popescu@reef.pl>
2025-01-08 14:12:17 +0000

    use staging image

--- validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/llm_prompts.py
+++ validator/app/src/compute_horde_validator/validator/synthetic_jobs/generator/llm_prompts.py
@@ -55,10 +55,10 @@ class LlmPromptsJobGenerator(BaseSyntheticJobGenerator):
         return 54  # it takes around 42s - we add 30% buffer
 
     def base_docker_image_name(self) -> str:
-        return "docker.io/backenddevelopersltd/compute-horde-prompt-solver:v0-latest"
+        return "docker.io/backenddevelopersltd/compute-horde-prompt-solver-staging:v0-latest"
 
     def docker_image_name(self) -> str:
-        return "docker.io/backenddevelopersltd/compute-horde-prompt-solver:v0-latest"
+        return "docker.io/backenddevelopersltd/compute-horde-prompt-solver-staging:v0-latest"
 
     def docker_run_options_preset(self) -> DockerRunOptionsPreset:
         return "nvidia_all"
