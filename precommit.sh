4c8bc704b6f8faf83b6ae97c33ab225d1b477604
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-01-17 18:27:38 +0100

    convenience pre-commit script

---
+++ precommit.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+for projectdir in compute_horde executor miner validator; do
+  cd "${projectdir}"
+  uv sync --all-groups
+  uv run ruff check --fix
+  uv run ruff format
+  uv run nox -s type_check lint
+  cd ".."
+done
+
+uv sync --all-groups
