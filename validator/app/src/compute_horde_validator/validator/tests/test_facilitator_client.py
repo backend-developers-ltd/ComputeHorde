136d9b666085dfdaae89e793177122b75d4d8b36
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-02-01 00:25:06 +0100

    (wip) exclude busy miners when picking miner for organic job

--- validator/app/src/compute_horde_validator/validator/tests/test_facilitator_client.py
+++ validator/app/src/compute_horde_validator/validator/tests/test_facilitator_client.py
@@ -15,6 +15,9 @@ from compute_horde.fv_protocol.validator_requests import (
     V0AuthenticationRequest,
     V0MachineSpecsUpdate,
 )
+from compute_horde.receipts.models import JobStartedReceipt
+from compute_horde.receipts.schemas import JobStartedReceiptPayload, Receipt
+from compute_horde.utils import sign_blob
 from django.utils import timezone
 
 from compute_horde_validator.validator.models import (
@@ -302,54 +305,61 @@ class FacilitatorExpectMachineSpecsWs(FacilitatorWs):
 
 @pytest.mark.django_db(databases=["default", "default_alias"], transaction=True)
 @pytest.mark.asyncio
-async def test_fetch_miner_for_cross_validation__wrap_around():
-    await setup_db(n := 5)
-
-    async with async_patch_all():
-        client = FacilitatorClient(get_keypair(), "ws://127.0.0.1:0/")
-
-        # loop twice to check wrap-around behavior
-        for i in range(n * 2):
-            miner = await client.fetch_miner_for_cross_validation(DEFAULT_EXECUTOR_CLASS)
-            expected_hotkey = f"miner_{i % n}"
-            assert miner.hotkey == expected_hotkey
-            assert client.last_miner_cross_validated == expected_hotkey
-
-        cancel_facilitator_tasks(client)
-
-
-@pytest.mark.django_db(databases=["default", "default_alias"], transaction=True)
-@pytest.mark.asyncio
-async def test_fetch_miner_for_cross_validation__no_matching_executor_class():
+async def test_pick_miner_for_job__no_matching_executor_class():
     await setup_db()
 
     async with async_patch_all():
         client = FacilitatorClient(get_keypair(), "ws://127.0.0.1:0/")
-        client.last_miner_cross_validated = "some_miner"
 
-        miner = await client.fetch_miner_for_cross_validation("NonExistentExecutorClass")
+        miner = await client.pick_miner_for_job("NonExistentExecutorClass")
         assert miner is None
-        assert client.last_miner_cross_validated is None
 
-        cancel_facilitator_tasks(client)
+        await cancel_facilitator_tasks(client)
 
 
 @pytest.mark.django_db(databases=["default", "default_alias"], transaction=True)
 @pytest.mark.asyncio
-async def test_fetch_miner_for_cross_validation__no_online_executors():
+async def test_pick_miner_for_job__no_online_executors():
     await setup_db()
     await MinerManifest.objects.all().aupdate(online_executor_count=0)
 
     async with async_patch_all():
         client = FacilitatorClient(get_keypair(), "ws://127.0.0.1:0/")
-        client.last_miner_cross_validated = "some_miner"
 
-        miner = await client.fetch_miner_for_cross_validation(DEFAULT_EXECUTOR_CLASS)
+        miner = await client.pick_miner_for_job(DEFAULT_EXECUTOR_CLASS)
         assert miner is None
-        assert client.last_miner_cross_validated is None
 
-        cancel_facilitator_tasks(client)
+        await cancel_facilitator_tasks(client)
 
+
+@pytest.mark.django_db(databases=["default", "default_alias"], transaction=True)
+@pytest.mark.asyncio
+async def test_pick_miner_for_job__all_executors_busy(validator_keypair, miner_keypair):
+    await setup_db()
+    async for miner in Miner.objects.all():
+        for _ in range(5):
+            payload = JobStartedReceiptPayload(
+                job_uuid=str(uuid.uuid4()),
+                miner_hotkey=miner.hotkey,
+                validator_hotkey=validator_keypair.ss58_address,
+                timestamp=timezone.now(),
+                executor_class=DEFAULT_EXECUTOR_CLASS,
+                max_timeout=60,
+                is_organic=True,
+                ttl=60,
+            )
+            blob = payload.blob_for_signing()
+            receipt = Receipt(
+                payload=payload,
+                validator_signature=sign_blob(validator_keypair, blob),
+                miner_signature=sign_blob(miner_keypair, blob),
+            )
+            await JobStartedReceipt.from_receipt(receipt).asave()
+    async with async_patch_all():
+        client = FacilitatorClient(get_keypair(), "ws://127.0.0.1:0/")
+        miner = await client.pick_miner_for_job(DEFAULT_EXECUTOR_CLASS)
+        assert miner is None
+    
 
 # TODO: this test is flaky, needs proper investigation
 @pytest.mark.skip
