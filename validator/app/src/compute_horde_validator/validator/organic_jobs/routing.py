43674f34a0594bc59f3db605c241a1a7542d1b01
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-02-02 02:37:00 +0100

    extract sub-functions for routing

--- validator/app/src/compute_horde_validator/validator/organic_jobs/routing.py
+++ validator/app/src/compute_horde_validator/validator/organic_jobs/routing.py
@@ -29,54 +29,56 @@ class AllMinersBusy(JobRoutingException):
 
 async def pick_miner_for_job(request: JobRequest) -> Miner:
     if isinstance(request, V0JobRequest | V1JobRequest):
-        """
-        V0 and V1 specify the miner in the job request itself - so just return that.
-        """
-        miner, _ = await Miner.objects.aget_or_create(hotkey=request.miner_hotkey)
-        return miner
+        return await pick_miner_for_job_v0_v1(request)
 
     if isinstance(request, V2JobRequest):
-        """
-        Goes through all miners with recent manifests and online executors of the given executor class
-        And returns a random miner that may have a non-busy executor based on known receipts.
-        """
-        executor_class = request.executor_class
-        now = timezone.now()
-
-        manifests_qs = MinerManifest.objects.select_related("miner").filter(
-            executor_class=str(executor_class),
-            online_executor_count__gt=0,
-            created_at__gte=timezone.now() - timedelta(hours=4),
-        )
-
-        manifests = [manifest async for manifest in manifests_qs.all()]
-
-        if not manifests:
-            raise NoMinerForExecutorType()
-
-        running_miner_jobs_counts_qs = (
-            JobStartedReceipt.objects.valid_at(now)
-            .filter(executor_class=executor_class)
-            .values("miner_hotkey")
-            .annotate(count=Count("*"))
-            .values_list("miner_hotkey", "count")
-        )
-
-        running_miner_jobs_counts: dict[str, int] = {
-            hotkey: count async for hotkey, count in running_miner_jobs_counts_qs
-        }
-
-        manifests = [
-            manifest
-            for manifest in manifests
-            if manifest.online_executor_count
-            > running_miner_jobs_counts.get(manifest.miner.hotkey, 0)
-        ]
-
-        if not manifests:
-            raise AllMinersBusy()
-
-        selected = random.choice(manifests)
-        return selected.miner
+        return await pick_miner_for_job_v2(request)
 
     assert_never(request)
+
+
+async def pick_miner_for_job_v2(request: V2JobRequest) -> Miner:
+    """
+    Goes through all miners with recent manifests and online executors of the given executor class.
+    Returns a random miner that may have a non-busy executor based on known receipts.
+    """
+    executor_class = request.executor_class
+    now = timezone.now()
+
+    manifests_qs = MinerManifest.objects.select_related("miner").filter(
+        executor_class=str(executor_class),
+        online_executor_count__gt=0,
+        created_at__gte=timezone.now() - timedelta(hours=4),
+    )
+    manifests = [manifest async for manifest in manifests_qs.all()]
+    if not manifests:
+        raise NoMinerForExecutorType()
+
+    running_miner_jobs_counts_qs = (
+        JobStartedReceipt.objects.valid_at(now)
+        .filter(executor_class=executor_class)
+        .values("miner_hotkey")
+        .annotate(count=Count("*"))
+        .values_list("miner_hotkey", "count")
+    )
+    running_miner_jobs_counts: dict[str, int] = {
+        hotkey: count async for hotkey, count in running_miner_jobs_counts_qs
+    }
+    manifests = [
+        manifest
+        for manifest in manifests
+        if manifest.online_executor_count > running_miner_jobs_counts.get(manifest.miner.hotkey, 0)
+    ]
+    if not manifests:
+        raise AllMinersBusy()
+
+    selected = random.choice(manifests)
+    return selected.miner
+
+
+async def pick_miner_for_job_v0_v1(request: V0JobRequest | V1JobRequest) -> Miner:
+    """
+    V0 and V1 specify the miner in the job request itself - so just return that.
+    """
+    miner, _ = await Miner.objects.aget_or_create(hotkey=request.miner_hotkey)
+    return miner
