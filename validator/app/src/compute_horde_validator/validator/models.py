c44f5854fe01024d1728a1a887c3b83e18c60950
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-02-10 21:45:34 +0100

    evict expired MinerBlacklist entries

--- validator/app/src/compute_horde_validator/validator/models.py
+++ validator/app/src/compute_horde_validator/validator/models.py
@@ -168,10 +168,6 @@ class MinerBlacklist(models.Model):
     def __str__(self):
         return f"hotkey: {self.miner.hotkey}"
 
-    @classmethod
-    async def acleanup(cls):
-        await cls.objects.expired().adelete()
-
 
 class ValidatorWhitelist(models.Model):
     hotkey = models.CharField(max_length=255, unique=True)
