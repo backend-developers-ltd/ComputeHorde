2dc7ca9ca2f1154dc61f167fa391707f6abd0804
Sławomir Górawski <slawomir.gorawski@reef.pl>
2025-02-27 15:41:29 +0100

    Fix InlineVolume printing long contents in logs

--- compute_horde/compute_horde/base/volume.py
+++ compute_horde/compute_horde/base/volume.py
@@ -37,7 +37,7 @@ class HuggingfaceVolume(pydantic.BaseModel):
 
 class InlineVolume(pydantic.BaseModel):
     volume_type: Literal[VolumeType.inline] = VolumeType.inline
-    contents: str
+    contents: str = Field(repr=False)
     relative_path: str | None = None
 
     def is_safe(self) -> bool:
