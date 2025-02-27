7ecc38daca86a2155d3bcef9ec302e310d8caed8
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2025-02-27 20:16:12 +0200

    remove commented out executor classes

--- compute_horde/compute_horde/executor_class.py
+++ compute_horde/compute_horde/executor_class.py
@@ -7,9 +7,6 @@ class ExecutorClass(StrEnum):
     spin_up_4min__gpu_24gb = "spin_up-4min.gpu-24gb"
     always_on__gpu_24gb = "always_on.gpu-24gb"
     always_on__llm__a6000 = "always_on.llm.a6000"
-    # always_on__cpu_16c__ram_64gb = "always_on.cpu-16c.ram-64gb"
-    # always_on__gpu_80gb = "always_on.gpu-80gb"
-    # always_on__gpu_24gb__docker_cached_facilitator = "always_on.gpu-24gb.docker_cached-facilitator"
 
 
 @dataclass
@@ -46,25 +43,6 @@ EXECUTOR_CLASS = {
         gpu_vram_gb=48,
         spin_up_time=int(timedelta(minutes=4).total_seconds()),
     ),
-    # ExecutorClass.always_on__cpu_16c__ram_64gb: ExecutorClassSpec(
-    #     cpu_cores=16,
-    #     ram_gb=64,
-    #     spin_up_time=0,
-    # ),
-    # ExecutorClass.always_on__gpu_80gb: ExecutorClassSpec(
-    #     has_gpu=True,
-    #     gpu_vram_gb=80,
-    #     spin_up_time=0,
-    # ),
-    # ExecutorClass.always_on__gpu_24gb__docker_cached_facilitator: ExecutorClassSpec(
-    #     has_gpu=True,
-    #     gpu_vram_gb=24,
-    #     spin_up_time=0,
-    #     docker_cached_images=[
-    #         "backenddevelopersltd/compute-horde-job-huggingface",
-    #         "backenddevelopersltd/compute-horde-job-ffmpeg:vidstab-v0",
-    #     ],
-    # ),
 }
 
 
