ec69c5c74cabaaa64358d0f5c622b4eeb9a41fc7
Enam Mijbah Noor <enam.noor@reef.pl>
2025-03-28 05:13:00 +0600

    facilitator: Remove html views

--- facilitator/app/src/project/core/urls.py
+++ facilitator/app/src/project/core/urls.py
@@ -1,32 +1,13 @@
 from django.urls import include, path
-from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework.schemas import get_schema_view
 
 from .api import router
 from .views import (
-    DockerImageJobCreateView,
-    JobDetailView,
-    JobListView,
-    RawScriptJobCreateView,
-    api_token_view,
     auth_login_view,
     auth_nonce_view,
 )
 
 urlpatterns = [
-    path("", JobListView.as_view(), name="job/list"),
-    path("new-docker/", DockerImageJobCreateView.as_view(), name="job-docker/submit"),
-    path("new-raw/", RawScriptJobCreateView.as_view(), name="job-raw/submit"),
-    path("<uuid:pk>/", JobDetailView.as_view(), name="job/detail"),
     path("api/v1/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
-    path("api-token-generate/", api_token_view, name="api-token-generate"),
-    path(
-        "api/v1/openapi",
-        get_schema_view(title="Compute Horde Facilitator", description="API creating organic jobs", version="1.0.0"),
-        name="openapi-schema",
-    ),
     path("auth/nonce", auth_nonce_view, name="auth_nonce"),
     path("auth/login", auth_login_view, name="auth_login"),
 ]
--- facilitator/app/src/project/urls.py
+++ facilitator/app/src/project/urls.py
@@ -1,17 +1,13 @@
 from django.conf import settings
 from django.contrib.admin.sites import site as admin_site
 from django.urls import include, path
-from fingerprint.views import FingerprintView
 
 from .core.business_metrics import metrics_manager
 from .core.consumers import ValidatorConsumer
 from .core.metrics import metrics_view
-from .core.views import SignupView
 
 urlpatterns = [
     path("admin/", admin_site.urls),
-    path("redirect/", FingerprintView.as_view(), name="fingerprint"),
-    path("accounts/signup/", SignupView.as_view(), name="account_signup"),
     path("accounts/", include("allauth.urls")),
     path("", include("project.core.urls")),
     path("metrics", metrics_view, name="prometheus-django-metrics"),
