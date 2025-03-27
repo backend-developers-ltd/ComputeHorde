5dca0039b66218698477c13d5c7b628b85c5d597
Enam Mijbah Noor <enam.noor@reef.pl>
2025-03-28 05:13:00 +0600

    facilitator: Remove html views

--- facilitator/app/src/project/core/urls.py
+++ facilitator/app/src/project/core/urls.py
@@ -1,28 +1,7 @@
 from django.urls import include, path
-from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework.schemas import get_schema_view
 
 from .api import router
-from .views import (
-    DockerImageJobCreateView,
-    JobDetailView,
-    JobListView,
-    RawScriptJobCreateView,
-    api_token_view,
-)
 
 urlpatterns = [
-    path("", JobListView.as_view(), name="job/list"),
-    path("new-docker/", DockerImageJobCreateView.as_view(), name="job-docker/submit"),
-    path("new-raw/", RawScriptJobCreateView.as_view(), name="job-raw/submit"),
-    path("<uuid:pk>/", JobDetailView.as_view(), name="job/detail"),
     path("api/v1/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
-    path("api-token-generate/", api_token_view, name="api-token-generate"),
-    path(
-        "api/v1/openapi",
-        get_schema_view(title="Compute Horde Facilitator", description="API creating organic jobs", version="1.0.0"),
-        name="openapi-schema",
-    ),
 ]
