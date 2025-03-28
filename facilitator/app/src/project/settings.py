55fca67571297a680dde888db843bc31a43d80c4
Enam Mijbah Noor <enam.noor@reef.pl>
2025-03-28 07:22:30 +0600

    facilitator: Disable browsable API

--- facilitator/app/src/project/settings.py
+++ facilitator/app/src/project/settings.py
@@ -422,7 +422,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
