c054397daac529f6c302dfa94b7772b3a23cd077
Enam Mijbah Noor <enam.noor@reef.pl>
2025-03-28 06:06:30 +0600

    facilitator: Remove raw_script field
    This field was only used in the HTML views. Since those views are removed, we remove the raw_script field as well.

--- facilitator/app/src/project/core/tests/test_api.py
+++ facilitator/app/src/project/core/tests/test_api.py
@@ -68,23 +68,15 @@ def job_docker(db, user, connected_validator, miner):
 
 
 @pytest.fixture
-def job_raw(db, user, connected_validator, miner):
-    return Job.objects.create(
-        user=user,
-        validator=connected_validator,
-        miner=miner,
-        raw_script="print(1)",
-        input_url="http://example.com/input.zip",
-    )
-
-
-@pytest.fixture
-def another_user_job_raw(db, another_user, connected_validator, miner):
+def another_user_job_docker(db, another_user, connected_validator, miner):
     return Job.objects.create(
         user=another_user,
         validator=connected_validator,
         miner=miner,
-        raw_script="print(1)",
+        docker_image="hello-world",
+        args=["my", "args"],
+        env={"MY_ENV": "my value"},
+        use_gpu=True,
         input_url="http://example.com/input.zip",
     )
 
@@ -102,7 +94,6 @@ def check_docker_job(job_result):
         "output_download_url",
     }
     assert job_result["docker_image"] == "hello-world"
-    assert job_result["raw_script"] == ""
     assert job_result["args"] == ["my", "args"]
     assert job_result["env"] == {"MY_ENV": "my value"}
     assert job_result["use_gpu"] is True
@@ -110,44 +101,26 @@ def check_docker_job(job_result):
     assert set(job_result.keys()) & generated_fields == generated_fields
 
 
-def check_raw_job(job_result):
-    generated_fields = {
-        "created_at",
-        "last_update",
-        "status",
-        "output_download_url",
-    }
-    assert job_result["raw_script"] == "print(1)"
-    assert job_result["docker_image"] == ""
-    assert job_result["args"] == []
-    assert job_result["env"] == {}
-    assert job_result["use_gpu"] is False
-    assert job_result["input_url"] == "http://example.com/input.zip"
-    assert set(job_result.keys()) & generated_fields == generated_fields
-
-
 @pytest.mark.django_db
-def test_job_viewset_list(api_client, user, job_docker, job_raw):
+def test_job_viewset_list(api_client, user, job_docker, another_user_job_docker):
     api_client.force_authenticate(user=user)
     response = api_client.get("/api/v1/jobs/")
     assert response.status_code == 200
-    assert len(response.data["results"]) == 2
+    assert len(response.data["results"]) == 1
 
-    docker_result = [job for job in response.data["results"] if job["uuid"] == str(job_docker.uuid)][0]
-    raw_result = [job for job in response.data["results"] if job["uuid"] == str(job_raw.uuid)][0]
-    check_docker_job(docker_result)
-    check_raw_job(raw_result)
+    for job_result in response.data["results"]:
+        check_docker_job(job_result)
 
 
 @pytest.mark.django_db
-def test_job_viewset_list_object_permissions(api_client, user, job_docker, job_raw, another_user_job_raw):
+def test_job_viewset_list_object_permissions(api_client, user, job_docker, another_user_job_docker):
     api_client.force_authenticate(user=user)
     response = api_client.get("/api/v1/jobs/")
     assert response.status_code == 200
-    assert len(response.data["results"]) == 2
+    assert len(response.data["results"]) == 1
 
     uuids = {job["uuid"] for job in response.data["results"]}
-    assert uuids == {str(job_docker.uuid), str(job_raw.uuid)}
+    assert uuids == {str(job_docker.uuid)}
 
 
 @pytest.mark.django_db
@@ -156,28 +129,6 @@ def test_job_viewset_retrieve_docker(api_client, user, job_docker):
     response = api_client.get(f"/api/v1/jobs/{job_docker.uuid}/")
     assert response.status_code == 200
     check_docker_job(response.data)
-
-
-@pytest.mark.django_db
-def test_job_viewset_retrieve_raw(api_client, user, job_raw):
-    api_client.force_authenticate(user=user)
-    response = api_client.get(f"/api/v1/jobs/{job_raw.uuid}/")
-    assert response.status_code == 200
-    check_raw_job(response.data)
-
-
-@pytest.mark.django_db
-def test_raw_job_viewset_create(api_client, user, connected_validator, miner):
-    api_client.force_authenticate(user=user)
-    data = {"raw_script": "print(1)", "input_url": "http://example.com/input.zip"}
-    response = api_client.post("/api/v1/job-raw/", data)
-    assert response.status_code == 201
-    assert Job.objects.count() == 1
-    job = Job.objects.first()
-    assert job.raw_script == "print(1)"
-    assert job.input_url == "http://example.com/input.zip"
-    assert job.use_gpu is False
-    assert job.user == user
 
 
 @pytest.mark.django_db
@@ -237,10 +188,10 @@ def build_http_headers(headers: dict[str, str]) -> dict[str, str]:
 
 @pytest.mark.django_db
 def test_hotkey_authentication__job_create(api_client, wallet, whitelisted_hotkey, connected_validator, miner):
-    data = {"raw_script": "print(1)", "input_url": "http://example.com/input.zip"}
+    data = {"docker_image": "hello-world", "input_url": "http://example.com/input.zip"}
     # First call without any authentication must return 401.
-    response = api_client.post("/api/v1/job-raw/", data)
-    assert response.status_code == 401
+    response = api_client.post("/api/v1/job-docker/", data)
+    assert response.status_code == 401, response.content
 
     # Create an active validator for this wallet.
     Validator.objects.create(ss58_address=wallet.hotkey.ss58_address, is_active=True)
@@ -255,14 +206,14 @@ def test_hotkey_authentication__job_create(api_client, wallet, whitelisted_hotke
 
     signed_headers = generate_signed_headers(
         wallet=wallet,
-        url="http://testserver/api/v1/job-raw/",
+        url="http://testserver/api/v1/job-docker/",
         method="POST",
         subnet_id=12,
     )
     signed_headers["Authorization"] = f"Bearer {token}"
 
     response = api_client.post(
-        "/api/v1/job-raw/",
+        "/api/v1/job-docker/",
         data,
         **build_http_headers(signed_headers),
     )
@@ -273,7 +224,7 @@ def test_hotkey_authentication__job_create(api_client, wallet, whitelisted_hotke
     token = get_auth_token(api_client, wallet)
     signed_headers["Authorization"] = f"Bearer {token}"
     response = api_client.post(
-        "/api/v1/job-raw/",
+        "/api/v1/job-docker/",
         data,
         **build_http_headers(signed_headers),
     )
@@ -282,7 +233,7 @@ def test_hotkey_authentication__job_create(api_client, wallet, whitelisted_hotke
     # Verify job creation.
     assert Job.objects.count() == 1
     job = Job.objects.first()
-    assert job.raw_script == "print(1)"
+    assert job.docker_image == "hello-world"
     assert job.input_url == "http://example.com/input.zip"
     assert job.use_gpu is False
     assert job.user is None
