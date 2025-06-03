bd3e8f0b24435f1d784b094ee9671b1d79caf0d4
Tomasz Żyźniewski <zyzniewski@gmail.com>
2025-06-03 12:34:36 +0200

    disable rich.traceback

--- compute_horde/compute_horde/base/__init__.py
+++ compute_horde/compute_horde/base/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-if os.environ.get("RICH_TRACEBACK", "") == "0":
+if os.environ.get("RICH_TRACEBACK", "0") == "0":
     import rich.traceback
 
     rich.traceback.install = lambda *a, **kw: None
