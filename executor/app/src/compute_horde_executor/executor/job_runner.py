4b318b606f542e5815aa1c35a7312db56543f920
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-07-03 21:15:54 +0200

    streamlined JobDriver for error handling

--- executor/app/src/compute_horde_executor/executor/job_runner.py
+++ executor/app/src/compute_horde_executor/executor/job_runner.py
@@ -12,7 +12,7 @@ from contextlib import asynccontextmanager
 from typing import Any
 
 from compute_horde.base.docker import DockerRunOptionsPreset
-from compute_horde.protocol_messages import V0InitialJobRequest, V0JobFailedRequest, V0JobRequest
+from compute_horde.protocol_messages import V0InitialJobRequest, V0JobRequest
 from compute_horde_core.certificate import (
     check_endpoint,
     generate_certificate_at,
@@ -25,7 +25,6 @@ from compute_horde_core.volume import (
     HuggingfaceVolume,
     Volume,
     VolumeDownloader,
-    VolumeDownloadFailed,
 )
 from django.conf import settings
 
@@ -439,7 +438,7 @@ class JobRunner:
         except Exception as e:
             logger.error(f"Failed to remove temp dir {self.temp_dir}: {e}")
 
-    async def unpack_volume(self):
+    async def download_volume(self):
         assert str(self.volume_mount_dir) not in {"~", "/"}
         for path in self.volume_mount_dir.glob("*"):
             if path.is_file():
@@ -455,20 +454,7 @@ class JobRunner:
             if volume_downloader.handles_volume_type() is HuggingfaceVolume:
                 if volume.token is None:
                     volume.token = settings.HF_ACCESS_TOKEN
-                try:
-                    await volume_downloader.download(self.volume_mount_dir)
-                except VolumeDownloadFailed as exc:
-                    logger.error(f"Failed to download model from Hugging Face: {exc}")
-                    raise JobError(
-                        str(exc),
-                        V0JobFailedRequest.ErrorType.HUGGINGFACE_DOWNLOAD,
-                        exc.error_detail,
-                    ) from exc
-            else:
-                try:
-                    await volume_downloader.download(self.volume_mount_dir)
-                except VolumeDownloadFailed as exc:
-                    raise JobError(str(exc)) from exc
+            await volume_downloader.download(self.volume_mount_dir)
 
         chmod_proc = await asyncio.create_subprocess_exec(
             "chmod", "-R", "777", self.temp_dir.as_posix()
