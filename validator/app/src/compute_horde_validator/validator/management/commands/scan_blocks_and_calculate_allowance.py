1bcff261d0f82850d59c38c155dd48a9a1e38bfb
Michał Nowacki <michal.nowacki@reef.pl>
2025-08-08 16:59:43 +0200

    allowance checkpoints changed

--- validator/app/src/compute_horde_validator/validator/management/commands/scan_blocks_and_calculate_allowance.py
+++ validator/app/src/compute_horde_validator/validator/management/commands/scan_blocks_and_calculate_allowance.py
@@ -1,7 +1,7 @@
 
 from django.core.management import BaseCommand
 
-from compute_horde_validator.validator.allowance.tasks import report_allowance_to_system_events
+from compute_horde_validator.validator.allowance.tasks import report_allowance_checkpoint
 from compute_horde_validator.validator.allowance.utils.blocks import (
     scan_blocks_and_calculate_allowance,
 )
@@ -12,6 +12,6 @@ from compute_horde_validator.validator.allowance.utils.supertensor_django_cache 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         scan_blocks_and_calculate_allowance(
-            report_allowance_to_system_events.delay,
+            report_allowance_checkpoint.delay,
             PrecachingSuperTensor(cache=DjangoCache()),
         )
