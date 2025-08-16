564cbccf526eddf5249b65bc873eeba16860db18
Andreea Popescu <andreea.popescu@reef.pl>
2025-08-16 09:28:02 +0100

    use sync pylon client

---
+++ validator/app/src/compute_horde_validator/validator/pylon.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from pylon_client import Hotkey, Neuron, PylonClient
+
+_pylon_client_instance: PylonClient | None = None
+
+
+def pylon_client() -> PylonClient:
+    global _pylon_client_instance
+    if _pylon_client_instance is None:
+        _pylon_client_instance = PylonClient(
+            base_url=f"http://{settings.PYLON_ADDRESS}:{settings.PYLON_PORT}"
+        )
+    return _pylon_client_instance
+
+
+def get_serving_hotkeys(neurons: list[Neuron]) -> list[Hotkey]:
+    """
+    Get serving hotkeys for a list of neurons.
+
+    Args:
+        neurons (list[Neuron]): List of Neuron objects.
+
+    Returns:
+        list[Hotkey]: List of Hotkey objects.
+    """
+    return [
+        neuron.hotkey for neuron in neurons if neuron.axon_info and neuron.axon_info.ip != "0.0.0.0"
+    ]
