5eee59774eadcab612e7e06db6469f56d8f07d5e
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2025-08-27 18:22:53 +0300

    routing: check receipts

--- validator/app/src/compute_horde_validator/validator/routing/default.py
+++ validator/app/src/compute_horde_validator/validator/routing/default.py
@@ -2,12 +2,13 @@ import asyncio
 import logging
 from typing import assert_never
 
-from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync, sync_to_async
 from compute_horde.fv_protocol.facilitator_requests import (
     OrganicJobRequest,
     V2JobRequest,
 )
 from django.conf import settings
+from django.utils import timezone
 
 from compute_horde_validator.validator.allowance.default import allowance
 from compute_horde_validator.validator.allowance.types import (
@@ -17,8 +18,8 @@ from compute_horde_validator.validator.allowance.types import (
 from compute_horde_validator.validator.allowance.types import (
     Miner as AllowanceMiner,
 )
-from compute_horde_validator.validator.allowance.utils.supertensor import supertensor
 from compute_horde_validator.validator.models import Miner
+from compute_horde_validator.validator.receipts.default import receipts
 from compute_horde_validator.validator.routing.base import RoutingBase
 from compute_horde_validator.validator.routing.types import (
     AllMinersBusy,
@@ -83,7 +84,7 @@ def _pick_miner_for_job_v2(request: V2JobRequest) -> JobRoute:
         request.download_time_limit + request.execution_time_limit + request.upload_time_limit
     )
 
-    current_block = supertensor().get_current_block()
+    current_block = allowance().get_current_block()
 
     # 1. Find miners with enough allowance
     try:
@@ -100,9 +101,41 @@ def _pick_miner_for_job_v2(request: V2JobRequest) -> JobRoute:
         raise
 
     miners = {miner.hotkey_ss58: miner for miner in allowance().miners()}
+    manifests = allowance().get_manifests()
 
     # 2. Iterate and try to reserve a miner
     for miner_hotkey, _ in suitable_miners:
+        started_receipts = async_to_sync(receipts().get_valid_job_started_receipts_for_miner)(
+            miner_hotkey, timezone.now()
+        )
+        known_started_jobs = {str(receipt.job_uuid) for receipt in started_receipts}
+
+        finished_receipts = async_to_sync(receipts().get_job_finished_receipts_for_miner)(
+            miner_hotkey, list(known_started_jobs)
+        )
+        known_finished_jobs = {str(receipt.job_uuid) for receipt in finished_receipts}
+
+        maybe_ongoing_jobs = known_started_jobs - known_finished_jobs
+
+        executor_dict = manifests.get(miner_hotkey, {})
+        executor_count = executor_dict.get(executor_class, 0)
+
+        if len(maybe_ongoing_jobs) >= executor_count:
+            logger.debug(
+                f"Skipping miner {miner_hotkey} with {executor_count} executors "
+                f"{len(known_started_jobs)} known started, "
+                f"{len(known_finished_jobs)} known finished, "
+                f"{len(maybe_ongoing_jobs)} maybe ongoing jobs at [{current_block}]"
+            )
+            continue
+
+        logger.info(
+            f"Picking miner {miner_hotkey} with {executor_count} executors "
+            f"{len(known_started_jobs)} known started, "
+            f"{len(known_finished_jobs)} known finished, "
+            f"{len(maybe_ongoing_jobs)} maybe ongoing jobs at [{current_block}]"
+        )
+
         try:
             # 3. Reserve allowance for this miner
             reservation_id, blocks = allowance().reserve_allowance(
