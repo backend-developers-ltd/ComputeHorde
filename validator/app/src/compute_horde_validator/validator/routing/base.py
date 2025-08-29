a3ffe333b68168d7d2b64c4abb7389514b15a935
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2025-08-29 14:11:42 +0300

    routing: updated docstring

--- validator/app/src/compute_horde_validator/validator/routing/base.py
+++ validator/app/src/compute_horde_validator/validator/routing/base.py
@@ -18,9 +18,5 @@ class RoutingBase(ABC):
     async def pick_miner_for_job_request(self, request: OrganicJobRequest) -> JobRoute:
         """
         Filters miners based on compute time allowance and minimum collateral requirements.
-        Creates a preliminary reservation for the selected miner and returns the miner with:
-        - Highest percentage of remaining allowance
-        - Highest collateral as a tiebreaker
-        - Available executors (less ongoing jobs than online executor count)
-        - Sufficient remaining time in the current cycle
+        Creates a reservation for the selected miner and returns a JobRoute with that miner.
         """
