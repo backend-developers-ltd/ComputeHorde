7385a7b49aa31799d26e70fb6bc1e1dd893214ae
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2025-09-04 12:14:30 +0300

    routing: MinerIncident model

--- validator/app/src/compute_horde_validator/validator/routing/types.py
+++ validator/app/src/compute_horde_validator/validator/routing/types.py
@@ -1,4 +1,5 @@
 import pydantic
+from django.db import models
 
 from compute_horde_validator.validator.allowance.types import Miner, block_ids, reservation_id
 
@@ -15,3 +16,9 @@ class JobRoute(pydantic.BaseModel):
     miner: Miner
     allowance_blocks: block_ids | None
     allowance_reservation_id: reservation_id | None
+
+
+class MinerIncidentType(models.TextChoices):
+    MINER_JOB_REJECTED = "MINER_JOB_REJECTED"
+    MINER_JOB_FAILED = "MINER_JOB_FAILED"
+    MINER_HORDE_FAILED = "MINER_HORDE_FAILED"
