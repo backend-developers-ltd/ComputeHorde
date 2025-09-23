5ad6d62716c82563d3736f06ff9b1c5a9101f45d
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-09-23 15:52:47 +0200

    feat: django admin views for browsing manifests, allowances and bookings

--- validator/app/src/compute_horde_validator/validator/admin.py
+++ validator/app/src/compute_horde_validator/validator/admin.py
@@ -5,8 +5,10 @@ from django.contrib import (
     admin,  # noqa
     messages,  # noqa
 )
+from django.contrib.admin import FieldListFilter
+from django.db.models import Count, OuterRef, Subquery, Sum
 from django.utils.safestring import mark_safe  # noqa
-from rangefilter.filters import DateTimeRangeFilter
+from rangefilter.filters import DateTimeRangeFilter, NumericRangeFilter
 
 from compute_horde_validator.validator.models import (
     AdminJobRequest,
@@ -21,6 +23,11 @@ from compute_horde_validator.validator.models import (
     SystemEvent,
     ValidatorWhitelist,
     Weights,
+)
+from compute_horde_validator.validator.models.allowance.internal import (
+    AllowanceBooking,
+    AllowanceMinerManifest,
+    BlockAllowance,
 )
 
 # noqa
@@ -170,6 +177,184 @@ class MinerBlacklistAdmin(admin.ModelAdmin):
         return super().get_queryset(request).select_related("miner")
 
 
+class NotNullFilter(FieldListFilter):
+    """
+    Filter for a nullable field.
+    Shows the following options:
+    - All: Show all records
+    - Present: Show records with the field not null
+    - Empty: Show records with the field null
+    """
+
+    def choices(self, changelist):
+        yield {
+            "selected": self.lookup_val is None,
+            "query_string": changelist.get_query_string(remove=[self.lookup_kwarg]),
+            "display": "All",
+        }
+        yield {
+            "selected": self.lookup_val == "yes",
+            "query_string": changelist.get_query_string({self.lookup_kwarg: "yes"}),
+            "display": "Present",
+        }
+        yield {
+            "selected": self.lookup_val == "no",
+            "query_string": changelist.get_query_string({self.lookup_kwarg: "no"}),
+            "display": "Empty",
+        }
+
+    def queryset(self, request, queryset):
+        if self.lookup_val == "yes":
+            return queryset.filter(**{f"{self.field_path}__isnull": False})
+        if self.lookup_val == "no":
+            return queryset.filter(**{f"{self.field_path}__isnull": True})
+        return queryset
+
+    def expected_parameters(self):
+        return [self.lookup_kwarg]
+
+    @property
+    def lookup_kwarg(self):
+        return f"has_{self.field_path}"
+
+    @property
+    def lookup_val(self):
+        return self.used_parameters.get(self.lookup_kwarg)
+
+
+class AllowanceMinerManifestAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = [
+        "block_number",
+        "miner_ss58address",
+        "executor_class",
+        "executor_count",
+        "success",
+        "is_drop",
+    ]
+    list_filter = [
+        "executor_class",
+        "success",
+        "is_drop",
+        ("executor_count", NumericRangeFilter),
+        ("block_number", NumericRangeFilter),
+    ]
+    search_fields = ["miner_ss58address"]
+    ordering = ["-block_number", "miner_ss58address", "executor_class"]
+
+
+class BlockAllowanceAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = [
+        "block_number",
+        "validator_ss58",
+        "miner_ss58",
+        "executor_class",
+        "allowance",
+        "is_booked",
+        "is_invalidated",
+    ]
+    list_filter = [
+        "executor_class",
+        ("block_id", NumericRangeFilter),
+        ("allowance", NumericRangeFilter),
+        ("allowance_booking", NotNullFilter),
+        ("invalidated_at_block", NotNullFilter),
+    ]
+    search_fields = ["validator_ss58", "miner_ss58"]
+    ordering = ["-block_id", "validator_ss58", "miner_ss58", "executor_class"]
+
+    def block_number(self, obj):
+        # Need to "alias" block_id as block_number or django will have trouble with sorting by an implicit field
+        return obj.block_id
+
+    block_number.admin_order_field = "block_id"
+    block_number.short_description = "Block"
+
+    def is_booked(self, obj):
+        return obj.allowance_booking is not None
+
+    is_booked.boolean = True
+
+    def is_invalidated(self, obj):
+        return obj.invalidated_at_block is not None
+
+    is_invalidated.boolean = True
+
+
+class BlockAllowanceInline(ReadOnlyAdminMixin, admin.TabularInline):
+    model = BlockAllowance
+    fields = [
+        "block_number",
+        "validator_ss58",
+        "miner_ss58",
+        "executor_class",
+        "allowance",
+        "invalidated_at_block",
+    ]
+    readonly_fields = ["block_number"]
+
+    def block_number(self, obj):
+        # Need to "alias" block_id as block_number or django will not see this field at all
+        return obj.block_id
+
+    block_number.short_description = "Block"
+
+
+class AllowanceBookingAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = [
+        "id",
+        "creation_timestamp",
+        "validator",
+        "miner",
+        "total_allowance",
+        "is_reserved",
+        "is_spent",
+        "block_count",
+        "reservation_expiry_time",
+    ]
+    list_filter = [
+        "is_reserved",
+        "is_spent",
+        ("creation_timestamp", DateTimeRangeFilter),
+        ("reservation_expiry_time", DateTimeRangeFilter),
+    ]
+    search_fields = ["blockallowance__validator_ss58", "blockallowance__miner_ss58"]
+    ordering = ["-creation_timestamp"]
+    inlines = [BlockAllowanceInline]
+
+    def get_queryset(self, request):
+        first_block_validator = Subquery(
+            BlockAllowance.objects.filter(allowance_booking=OuterRef("pk")).values(
+                "validator_ss58"
+            )[:1]
+        )
+        first_block_miner = Subquery(
+            BlockAllowance.objects.filter(allowance_booking=OuterRef("pk")).values("miner_ss58")[:1]
+        )
+
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(
+                block_count=Count("blockallowance"),
+                total_allowance=Sum("blockallowance__allowance"),
+                first_validator_ss58=first_block_validator,
+                first_miner_ss58=first_block_miner,
+            )
+        )
+
+    def validator(self, obj):
+        return obj.first_validator_ss58 or "(!) NO BOOKED BLOCKS"
+
+    def miner(self, obj):
+        return obj.first_miner_ss58 or "(!) NO BOOKED BLOCKS"
+
+    def block_count(self, obj):
+        return obj.block_count or 0
+
+    def total_allowance(self, obj):
+        return obj.total_allowance or 0
+
+
 admin.site.register(Miner, admin_class=MinerReadOnlyAdmin)
 admin.site.register(SyntheticJob, admin_class=JobReadOnlyAdmin)
 admin.site.register(OrganicJob, admin_class=JobReadOnlyAdmin)
@@ -182,3 +367,6 @@ admin.site.register(SolveWorkload, admin_class=SolveWorkloadAdmin)
 admin.site.register(PromptSample, admin_class=PromptSampleAdmin)
 admin.site.register(Prompt, admin_class=PromptAdmin)
 admin.site.register(ValidatorWhitelist, admin_class=ValidatorWhitelistAdmin)
+admin.site.register(AllowanceMinerManifest, admin_class=AllowanceMinerManifestAdmin)
+admin.site.register(BlockAllowance, admin_class=BlockAllowanceAdmin)
+admin.site.register(AllowanceBooking, admin_class=AllowanceBookingAdmin)
