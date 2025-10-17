007545caa3319a63db0fd110c4ad1ea68a5a6e70
piotr-figwer-reef <piotr.figwer@reef.pl>
2025-10-17 07:36:46 +0200

    fix: Reapply "refactor: Remove legacy metagraph fetcher (#728)" (#763)
    This reverts commit 6052931111d613636f2833335509e06dac6c94d0.
    
    Impacts: validator

--- validator/app/src/compute_horde_validator/validator/receipts/default.py
+++ validator/app/src/compute_horde_validator/validator/receipts/default.py
@@ -7,6 +7,7 @@ from collections.abc import Awaitable, Callable, Sequence
 from dataclasses import dataclass
 
 import aiohttp
+from asgiref.sync import sync_to_async
 from compute_horde.receipts.models import (
     JobAcceptedReceipt,
     JobFinishedReceipt,
@@ -28,9 +29,10 @@ from django.utils import timezone
 from prometheus_client import Counter, Gauge, Histogram
 from typing_extensions import deprecated
 
+from compute_horde_validator.validator.allowance.default import allowance
 from compute_horde_validator.validator.allowance.utils.supertensor import supertensor
 from compute_horde_validator.validator.dynamic_config import aget_config
-from compute_horde_validator.validator.models import MetagraphSnapshot, Miner
+from compute_horde_validator.validator.models import Miner
 from compute_horde_validator.validator.models.allowance.internal import Block
 
 from .base import ReceiptsBase
@@ -120,10 +122,17 @@ class Receipts(ReceiptsBase):
             logger.info("Will fetch receipts from metagraph snapshot miners")
 
             async def miners():
-                snapshot = await MetagraphSnapshot.aget_latest()
-                serving_hotkeys = snapshot.serving_hotkeys
-                serving_miners = [m async for m in Miner.objects.filter(hotkey__in=serving_hotkeys)]
-                return [(m.hotkey, m.address, m.port) for m in serving_miners]
+                def load_miners() -> list[MinerInfo]:
+                    metagraph = allowance().get_metagraph()
+                    serving_hotkeys = metagraph.serving_hotkeys
+                    return [
+                        (hotkey, address, port)
+                        for hotkey, address, port in Miner.objects.filter(
+                            hotkey__in=serving_hotkeys
+                        ).values_list("hotkey", "address", "port")
+                    ]
+
+                return await sync_to_async(load_miners)()
 
         # IMPORTANT: This encompasses at least the current and the previous cycle.
         cutoff = timezone.now() - datetime.timedelta(hours=5)
