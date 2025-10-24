345c5bc91c571deea139fd5d2c48f3b0790dbce3
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2025-10-24 14:30:53 +0300

    feat: install-executor.sh

---
+++ install_executor.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -euxo pipefail
+
+if [ $# -lt 1 ] || [ $# -gt 1 ];
+then
+  >&2 echo "USAGE: ./install_executor.sh SSH_DESTINATION"
+  exit 1
+fi
+
+SSH_DESTINATION="$1"
+
+# install necessary software in the server
+ssh "$SSH_DESTINATION" <<'ENDSSH'
+set -euxo pipefail
+export DEBIAN_FRONTEND=noninteractive
+
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUPpB7FW7pLpmJSOUy/5UJ+04YT6OIndh28TPhTttTT miner@executor" >> ~/.ssh/authorized_keys
+
+# install docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+do
+  (yes | sudo apt-get remove $pkg) || true
+done
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-compose-plugin
+sudo usermod -aG docker $USER
+
+# install cuda
+sudo apt-get install -y linux-headers-$(uname -r)
+DISTRIBUTION=$(. /etc/os-release; echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget "https://developer.download.nvidia.com/compute/cuda/repos/$DISTRIBUTION/x86_64/cuda-keyring_1.1-1_all.deb"
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+  | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+  | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+  | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl stop docker
+sudo systemctl start docker
+
+sudo shutdown -r now
+ENDSSH
