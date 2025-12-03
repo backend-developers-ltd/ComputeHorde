b3e5dfa8576efa72b1477f90e403ddf9c28251d0
Kordian Kowalski <kordian.kowalski@reef.pl>
2025-12-03 19:50:25 +0100

    fix: remove unused django.contrib.auth routes
    They explode with a 500 when accessed - a template file is missing.
    This raises error all the way up to sentry when a bot (presumably) tries
    to access it.
    We don't need these, they don't affect the admin.
    
    Impacts: validator, miner, executor

--- validator/app/src/compute_horde_validator/urls.py
+++ validator/app/src/compute_horde_validator/urls.py
@@ -9,7 +9,6 @@ from .validator.metrics import metrics_view
 urlpatterns = [
     path("admin/", site.urls),
     path("", RedirectView.as_view(url="/admin/"), name="home"),
-    path("", include("django.contrib.auth.urls")),
     path("metrics", metrics_view, name="prometheus-django-metrics"),
     path("business-metrics", metrics_manager.view, name="prometheus-business-metrics"),
 ]
