7afca851068226615b2eb8aa9dd634080d7dcd67
Michał Nowacki <michal.nowacki@reef.pl>
2026-01-20 16:59:16 +0100

    fix comment

--- compute_horde/compute_horde/test_base/pylon.py
+++ compute_horde/compute_horde/test_base/pylon.py
@@ -34,7 +34,6 @@ The way to use these convenience mocks in test cases is:
 >>>    ...   
 """
 
-#  This is a temporary solution until we have a proper test setup for PylonClient in pylon repo.
 
 @pytest.fixture
 def mock_pylon_client(mocker):
